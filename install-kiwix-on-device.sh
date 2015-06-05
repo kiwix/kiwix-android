ec958b3b53393889c2df794b1cb124ba4d44da84
Emmanuel Engelhart <kelson@kiwix.org>
2015-06-05 12:50:47 +0200

    More debug messages

--- install-kiwix-on-device.sh
+++ install-kiwix-on-device.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 if [ -f build/outputs/apk/android-debug-unaligned.apk ]
 then
+    echo "Uninstalling old Kiwix APK..."
     ../src/dependencies/android-sdk/platform-tools/adb uninstall org.kiwix.kiwixmobile ;
+    echo "Installing new Kiwix APK..."
     ../src/dependencies/android-sdk/platform-tools/adb install build/outputs/apk/android-debug-unaligned.apk
 else
     echo "No APK file available!"
