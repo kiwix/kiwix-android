415579839a268158694fed32e591e8045e79b3db
Rashiq <rashiq.z@gmail.com>
2016-06-06 01:31:45 +0200

    Add constants for the DownloadService

---
+++ src/org/kiwix/kiwixmobile/downloader/DownloadIntent.java
@@ -0,0 +1,6 @@
+package org.kiwix.kiwixmobile.downloader;
+
+public class DownloadIntent {
+  public static final String DOWNLOAD_URL_PARAMETER = "DOWNLOAD_URL_PARAMETER";
+  public static final int DOWNLOAD_NOTIFICATION_ID = 431996;
+}
