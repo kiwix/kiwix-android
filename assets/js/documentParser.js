60e3ca6af1f3fd293085dad7341534b9b1a8d75a
mhutti1 <mhutti1@gmail.com>
2017-01-06 22:53:56 +0000

    Fix Table of Contents for id less headers #424

--- assets/js/documentParser.js
+++ assets/js/documentParser.js
@@ -5,6 +5,9 @@ function() {
     window.DocumentParser.start();
     for (i = 0; i < document.querySelectorAll('h1, h2, h3, h4, h5, h6').length; i++) {
       headerObject = document.querySelectorAll('h1, h2, h3, h4, h5, h6')[i];
+      if (headerObject.id === "") {
+        headerObject.id = "documentparserid" + i;
+      }
       window.DocumentParser.parse(headerObject.textContent, headerObject.tagName, headerObject.id);
     }
     window.DocumentParser.stop();
