8c84688108916d5962a59cf2710149e9b505c679
Elad Keyshawn <elad.keyshawn@gmail.com>
2017-04-06 00:29:59 +0200

    basic mvp for old (bookmarks)

---
+++ app/src/main/java/org/kiwix/kiwixmobile/bookmarksView/Presenter.java
@@ -0,0 +1,11 @@
+package org.kiwix.kiwixmobile.bookmarksView;
+
+/**
+ * Created by EladKeyshawn on 05/04/2017.
+ */
+public interface Presenter<V extends ViewCallback> {
+
+    void attachView(V mvpView);
+
+    void detachView();
+}
