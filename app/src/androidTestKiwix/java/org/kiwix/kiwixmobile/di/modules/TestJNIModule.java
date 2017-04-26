561f7e3ace3d2db87696a90f43759cabba3ccf55
mhutti1 <mhutti1@gmail.com>
2017-04-26 15:55:40 +0200

    Mocked more JNI Methods

--- app/src/androidTestKiwix/java/org/kiwix/kiwixmobile/di/modules/TestJNIModule.java
+++ app/src/androidTestKiwix/java/org/kiwix/kiwixmobile/di/modules/TestJNIModule.java
@@ -2,6 +2,7 @@ package org.kiwix.kiwixmobile.di.modules;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.doAnswer;
 import static org.mockito.Mockito.doReturn;
 
 import android.content.Context;
@@ -12,7 +13,11 @@ import java.io.InputStream;
 import javax.inject.Singleton;
 import org.apache.commons.io.IOUtils;
 import org.kiwix.kiwixlib.JNIKiwix;
+import org.kiwix.kiwixlib.JNIKiwixString;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mockito;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
 
 /**
  * Created by mhutti1 on 13/04/17.
@@ -23,8 +28,23 @@ public class TestJNIModule{
 
   @Provides
   @Singleton
-  public JNIKiwix providesJNIKiwix(Context context) {
+  public JNIKiwix providesJNIKiwix() {
     JNIKiwix jniKiwix = Mockito.mock(JNIKiwix.class);
+
+    doReturn("A/index.htm").when(jniKiwix).getMainPage();
+    doReturn(true).when(jniKiwix).loadZIM(any());
+    doReturn(true).when(jniKiwix).loadFulltextIndex(any());
+    doReturn("mockid").when(jniKiwix).getId();
+    doReturn("mockname").when(jniKiwix).getName();
+    doReturn("Test Description").when(jniKiwix).getDescription();
+    doAnswer(invocation -> {
+      ((JNIKiwixString) invocation.getArgument(0)).value = "Test Title";
+      return true;
+    }).when(jniKiwix).getTitle(any());
+    doReturn("Test Publisher").when(jniKiwix).getPublisher();
+    doReturn("Test Date").when(jniKiwix).getDate();
+    doReturn("Test Language").when(jniKiwix).getLanguage();
+
     try {
       InputStream inStream = TestJNIModule.class.getClassLoader().getResourceAsStream("summary");
       byte[] summary = IOUtils.toByteArray(inStream);
@@ -32,14 +52,10 @@ public class TestJNIModule{
       byte[] fool = IOUtils.toByteArray(inStream2);
       doReturn(summary).when(jniKiwix).getContent(eq("A/index.htm"),any(),any());
       doReturn(fool).when(jniKiwix).getContent(eq("A/A_Fool_for_You.html"),any(),any());
-      doReturn("A/index.htm").when(jniKiwix).getMainPage();
-      doReturn(true).when(jniKiwix).loadZIM(any());
-      doReturn(true).when(jniKiwix).loadFulltextIndex(any());
-      doReturn("mockid").when(jniKiwix).getId();
-      doReturn("mockname").when(jniKiwix).getName();
     } catch (IOException e) {
       e.printStackTrace();
     }
+
     return jniKiwix;
   }
 
