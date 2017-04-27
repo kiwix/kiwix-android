865c535f8888cb5a03acb7de84b3698e405dff12
mhutti1 <mhutti1@gmail.com>
2017-04-27 15:53:45 +0200

    Extend test timeout

--- app/src/androidTestKiwix/java/org/kiwix/kiwixmobile/tests/DownloadTest.java
+++ app/src/androidTestKiwix/java/org/kiwix/kiwixmobile/tests/DownloadTest.java
@@ -19,13 +19,16 @@ import static org.kiwix.kiwixmobile.testutils.TestUtils.withContent;
 import static org.kiwix.kiwixmobile.utils.StandardActions.enterHelp;
 
 import android.support.test.espresso.Espresso;
+import android.support.test.espresso.IdlingPolicies;
 import android.support.test.espresso.ViewInteraction;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 import android.test.suitebuilder.annotation.LargeTest;
+import java.util.concurrent.TimeUnit;
 import javax.inject.Inject;
 import org.junit.After;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -43,6 +46,12 @@ public class DownloadTest {
   @Rule
   public ActivityTestRule<SplashActivity> mActivityTestRule = new ActivityTestRule<>(
       SplashActivity.class);
+
+  @BeforeClass
+  public static void beforeClass() {
+    IdlingPolicies.setMasterPolicyTimeout(350, TimeUnit.SECONDS);
+    IdlingPolicies.setIdlingResourceTimeout(350, TimeUnit.SECONDS);
+  }
 
   @Before
   public void setUp() {
