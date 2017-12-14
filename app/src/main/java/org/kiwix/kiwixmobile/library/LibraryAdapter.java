kiwix/kiwix-android

commit b9930252e1a540366d7ecae5e0336a22497bc92c

Fix library test content matcher

app/src/main/java/org/kiwix/kiwixmobile/library/LibraryAdapter.java
@@ -107,7 +107,7 @@ public class LibraryAdapter extends BaseAdapter {
 
   @Override
   public Object getItem(int i) {
-    return listItems.get(i);
+    return listItems.get(i).data;
   }
 
   @Override
@@ -118,6 +118,9 @@ public class LibraryAdapter extends BaseAdapter {
   @Override
   public View getView(int position, View convertView, ViewGroup parent) {
     ViewHolder holder;
+    if (position + 1 >= listItems.size()) {
+      return convertView;
+    }
     ListItem item = listItems.get(position);
 
     if (item.type == LIST_ITEM_TYPE_BOOK) {
@@ -248,9 +251,9 @@ public class LibraryAdapter extends BaseAdapter {
             .filter(book -> !LibraryFragment.downloadingBooks.contains(book))
             .toList().toBlocking().single();
 
-        listItems.add(new ListItem("In your language:", LIST_ITEM_TYPE_DIVIDER));
+        listItems.add(new ListItem(context.getResources().getString(R.string.your_languages), LIST_ITEM_TYPE_DIVIDER));
         addBooks(selectedLanguages);
-        listItems.add(new ListItem("In other languages:", LIST_ITEM_TYPE_DIVIDER));
+        listItems.add(new ListItem(context.getResources().getString(R.string.other_languages), LIST_ITEM_TYPE_DIVIDER));
         addBooks(unselectedLanguages);
       } else {
         List<Book> selectedLanguages = Observable.from(allBooks)

app/src/androidTest/java/org/kiwix/kiwixmobile/testutils/TestUtils.java
@@ -1,6 +1,7 @@
 package org.kiwix.kiwixmobile.testutils;
 
 import android.Manifest;
+import android.app.LauncherActivity;
 import android.content.pm.PackageManager;
 import android.os.Build;
 import android.support.test.InstrumentationRegistry;
@@ -42,13 +43,17 @@ public class TestUtils {
   }
 
   public static Matcher<Object> withContent(final String content) {
-    return new BoundedMatcher<Object, Book>(Book.class) {
+    return new BoundedMatcher<Object, Object>(Object.class) {
       @Override
-      public boolean matchesSafely(Book myObj) {
-        if (myObj.getUrl() != null) {
-          return myObj.getUrl().contains(content);
+      public boolean matchesSafely(Object myObj) {
+        if (!(myObj instanceof Book)) {
+          return false;
+        }
+        Book book = (Book) myObj;
+        if (book.getUrl() != null) {
+          return book.getUrl().contains(content);
         } else {
-          return myObj.file.getPath().contains(content);
+          return book.file.getPath().contains(content);
         }
       }
 

app/src/main/res/values/strings.xml
@@ -187,4 +187,6 @@
   <string name="external_link_popup_dialog_title">Entering External Link</string>
   <string name="external_link_popup_dialog_message">You are entering an external link. This could lead to additional costs for data transfer or will just not work when you are offline. Do you want to continue?</string>
   <string name="do_not_ask_anymore">Do not ask anymore</string>
+  <string name="your_languages">Selected languages:</string>
+  <string name="other_languages">Other languages:</string>
 </resources>

commit 6bb9ad5828904f1fc299eae4acb798d79eb0b093

Bug fix for #320 triggered when the library contained 1 item.
A sample library_zim.xml is available at
https://github.com/kiwix/testfiles/blob/master/libraryfiles/one_item_in_library/library_zim.xml

The bug seems to have been introduced inadvertently in
https://github.com/kiwix/kiwix-android/commit/b9930252e1a540366d7ecae5e0336a22497bc92c
which returned a null to `getView(...)`

I've left a key log statement as this was one clue (after checking
various other areas of the codebase) that indicated there might be a
problem related to the number of entries rather than the attributes of
the item. I aim to revise the format of the log message as part of a
wider update on logging.

app/src/main/java/org/kiwix/kiwixmobile/library/LibraryAdapter.java
@@ -24,6 +24,7 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
 import android.util.Base64;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -118,7 +119,7 @@ public class LibraryAdapter extends BaseAdapter {
   @Override
   public View getView(int position, View convertView, ViewGroup parent) {
     ViewHolder holder;
-    if (position + 1 >= listItems.size()) {
+    if (position >= listItems.size()) {
       return convertView;
     }
     ListItem item = listItems.get(position);

app/src/main/java/org/kiwix/kiwixmobile/zim_manager/library_view/LibraryFragment.java
@@ -20,6 +20,7 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AlertDialog;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -156,6 +157,7 @@ public class LibraryFragment extends Fragment
 
   @Override
   public void showBooks(LinkedList<Book> books) {
+    Log.i("kiwix-showBooks", "Contains:" + books.size());
     libraryAdapter.setAllBooks(books);
     if (faActivity.searchView != null) {
       libraryAdapter.getFilter().filter(