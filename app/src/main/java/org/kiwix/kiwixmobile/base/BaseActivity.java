2d40597ea1d69ec3539c8a172937b07844d60a9b
Harsh Patel <harshaudi27@gmail.com>
2018-02-28 16:17:28 +0530

    Final Base Activity
    The error which Travis was throwing is solved in this update.

--- app/src/main/java/org/kiwix/kiwixmobile/base/BaseActivity.java
+++ app/src/main/java/org/kiwix/kiwixmobile/base/BaseActivity.java
@@ -3,6 +3,7 @@ package org.kiwix.kiwixmobile.base;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 import org.kiwix.kiwixmobile.KiwixApplication;
 import org.kiwix.kiwixmobile.di.components.ApplicationComponent;
