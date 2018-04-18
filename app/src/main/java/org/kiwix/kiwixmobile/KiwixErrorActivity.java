76dc09016e21cb2ca490559cb929fc34794445c7
Joseph Reeve <joseph.reeve@googlemail.com>
2018-04-18 10:42:38 +0100

    Fixed formatting

--- app/src/main/java/org/kiwix/kiwixmobile/KiwixErrorActivity.java
+++ app/src/main/java/org/kiwix/kiwixmobile/KiwixErrorActivity.java
@@ -27,135 +27,135 @@ import static org.kiwix.kiwixmobile.utils.LanguageUtils.getCurrentLocale;
 
 public class KiwixErrorActivity extends AppCompatActivity {
 
-    @BindView(R.id.messageText)
-    TextView messageText;
+  @BindView(R.id.messageText)
+  TextView messageText;
 
-    @BindView(R.id.reportButton)
-    Button reportButton;
+  @BindView(R.id.reportButton)
+  Button reportButton;
 
-    @BindView(R.id.restartButton)
-    Button restartButton;
+  @BindView(R.id.restartButton)
+  Button restartButton;
 
-    @BindView(R.id.allowLanguage)
-    CheckBox allowLanguageCheckbox;
+  @BindView(R.id.allowLanguage)
+  CheckBox allowLanguageCheckbox;
 
-    @BindView(R.id.allowZims)
-    CheckBox allowZimsCheckbox;
+  @BindView(R.id.allowZims)
+  CheckBox allowZimsCheckbox;
 
-    @BindView(R.id.allowCrash)
-    CheckBox allowCrashCheckbox;
+  @BindView(R.id.allowCrash)
+  CheckBox allowCrashCheckbox;
 
-    @BindView(R.id.allowLogs)
-    CheckBox allowLogsCheckbox;
+  @BindView(R.id.allowLogs)
+  CheckBox allowLogsCheckbox;
 
-    @BindView(R.id.allowDeviceDetails)
-    CheckBox allowDeviceDetailsCheckbox;
+  @BindView(R.id.allowDeviceDetails)
+  CheckBox allowDeviceDetailsCheckbox;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_kiwix_error);
-        ButterKnife.bind(this);
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_kiwix_error);
+    ButterKnife.bind(this);
 
-        Intent callingIntent = getIntent();
+    Intent callingIntent = getIntent();
 
-        Bundle extras = callingIntent.getExtras();
-        Throwable exception = (Throwable) extras.getSerializable("exception");
+    Bundle extras = callingIntent.getExtras();
+    Throwable exception = (Throwable) extras.getSerializable("exception");
 
-        reportButton.setOnClickListener(v -> {
+    reportButton.setOnClickListener(v -> {
 
 
-            Intent emailIntent = new Intent(Intent.ACTION_SEND);
-            emailIntent.setType("vnd.android.cursor.dir/email");
-            String to[] = {"android-crash-feedback@kiwix.org"};
-            emailIntent.putExtra(Intent.EXTRA_EMAIL, to);
-            emailIntent.putExtra(Intent.EXTRA_SUBJECT, "Someone has reported a crash");
+      Intent emailIntent = new Intent(Intent.ACTION_SEND);
+      emailIntent.setType("vnd.android.cursor.dir/email");
+      String to[] = {"android-crash-feedback@kiwix.org"};
+      emailIntent.putExtra(Intent.EXTRA_EMAIL, to);
+      emailIntent.putExtra(Intent.EXTRA_SUBJECT, "Someone has reported a crash");
 
-            String body = "Hi Kiwix Developers!\n" +
-                    "The Android app crashed, here are some details to help fix it:\n\n";
+      String body = "Hi Kiwix Developers!\n" +
+          "The Android app crashed, here are some details to help fix it:\n\n";
 
-            if(allowLogsCheckbox.isChecked()) {
-                File appDirectory = new File(Environment.getExternalStorageDirectory() + "/Kiwix");
-                File logFile = new File(appDirectory, "logcat.txt");
-                Uri path = FileProvider.getUriForFile(this, getApplicationContext().getPackageName() + ".provider", logFile);
-                emailIntent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
-                emailIntent.putExtra(Intent.EXTRA_STREAM, path);
-            }
+      if(allowLogsCheckbox.isChecked()) {
+        File appDirectory = new File(Environment.getExternalStorageDirectory() + "/Kiwix");
+        File logFile = new File(appDirectory, "logcat.txt");
+        Uri path = FileProvider.getUriForFile(this, getApplicationContext().getPackageName() + ".provider", logFile);
+        emailIntent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+        emailIntent.putExtra(Intent.EXTRA_STREAM, path);
+      }
 
-            if(allowCrashCheckbox.isChecked()) {
-                body += "Exception Details:\n\n" +
-                        exception.toString() +
-                "\n\n";
-            }
+      if(allowCrashCheckbox.isChecked()) {
+        body += "Exception Details:\n\n" +
+            exception.toString() +
+        "\n\n";
+      }
 
-            if(allowZimsCheckbox.isChecked()) {
-                BookDao bookDao = new BookDao(KiwixDatabase.getInstance(getApplicationContext()));
-                ArrayList<LibraryNetworkEntity.Book> books = bookDao.getBooks();
+      if(allowZimsCheckbox.isChecked()) {
+        BookDao bookDao = new BookDao(KiwixDatabase.getInstance(getApplicationContext()));
+        ArrayList<LibraryNetworkEntity.Book> books = bookDao.getBooks();
 
-                StringBuilder sb = new StringBuilder();
-                for(LibraryNetworkEntity.Book book: books) {
-                    String bookString = book.getTitle() +
-                            ":\nArticles: ["+ book.getArticleCount() +
-                            "]\nCreator: [" + book.getCreator() +
-                            "]\n";
+        StringBuilder sb = new StringBuilder();
+        for(LibraryNetworkEntity.Book book: books) {
+          String bookString = book.getTitle() +
+              ":\nArticles: ["+ book.getArticleCount() +
+              "]\nCreator: [" + book.getCreator() +
+              "]\n";
 
-                    sb.append(bookString);
-                }
-                String allZimFiles = sb.toString();
+          sb.append(bookString);
+        }
+        String allZimFiles = sb.toString();
 
-                String currentZimFile = ZimContentProvider.getZimFile();
-                body += "Curent Zim File:\n" +
-                        currentZimFile +
-                        "\n\nAll Zim Files in DB:\n" +
-                        allZimFiles +
-                "\n\n";
-            }
+        String currentZimFile = ZimContentProvider.getZimFile();
+        body += "Curent Zim File:\n" +
+            currentZimFile +
+            "\n\nAll Zim Files in DB:\n" +
+            allZimFiles +
+        "\n\n";
+      }
 
-            if(allowLanguageCheckbox.isChecked()) {
-                body += "Current Locale:\n" +
-                        getCurrentLocale(getApplicationContext()) +
-                "\n\n";
-            }
+      if(allowLanguageCheckbox.isChecked()) {
+        body += "Current Locale:\n" +
+            getCurrentLocale(getApplicationContext()) +
+        "\n\n";
+      }
 
-            if(allowDeviceDetailsCheckbox.isChecked()) {
-                body += "Device Details:\n" +
-                        "Device:[" + Build.DEVICE
-                        + "]\nModel:[" + Build.MODEL
-                        + "]\nManufacturer:[" + Build.MANUFACTURER
-                        + "]\nTime:[" + Build.TIME
-                        + "]\nAndroid Version:[" + Build.VERSION.RELEASE
-                        + "]" +
-                        "\n\n";
-            }
+      if(allowDeviceDetailsCheckbox.isChecked()) {
+        body += "Device Details:\n" +
+            "Device:[" + Build.DEVICE
+            + "]\nModel:[" + Build.MODEL
+            + "]\nManufacturer:[" + Build.MANUFACTURER
+            + "]\nTime:[" + Build.TIME
+            + "]\nAndroid Version:[" + Build.VERSION.RELEASE
+            + "]" +
+            "\n\n";
+      }
 
-            emailIntent.putExtra(Intent.EXTRA_TEXT, body);
+      emailIntent.putExtra(Intent.EXTRA_TEXT, body);
 
-            startActivityForResult(Intent.createChooser(emailIntent , "Send email..."), 1);
-        });
+      startActivityForResult(Intent.createChooser(emailIntent , "Send email..."), 1);
+    });
 
-        restartButton.setOnClickListener(v -> restartApp());
-    }
+    restartButton.setOnClickListener(v -> restartApp());
+  }
 
-    void restartApp(){
-        Context context = KiwixErrorActivity.this;
-        Intent intent = new Intent(context, SplashActivity.class);
-        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_RESET_TASK_IF_NEEDED);
-        intent.setAction(Intent.ACTION_MAIN);
-        intent.addCategory(Intent.CATEGORY_LAUNCHER);
-        context.startActivity(intent);
+  void restartApp(){
+    Context context = KiwixErrorActivity.this;
+    Intent intent = new Intent(context, SplashActivity.class);
+    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_RESET_TASK_IF_NEEDED);
+    intent.setAction(Intent.ACTION_MAIN);
+    intent.addCategory(Intent.CATEGORY_LAUNCHER);
+    context.startActivity(intent);
 
-        finish();
-        killCurrentProcess();
-    }
+    finish();
+    killCurrentProcess();
+  }
 
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data){
-        super.onActivityResult(requestCode, resultCode, data);
-        restartApp();
-    }
+  @Override
+  public void onActivityResult(int requestCode, int resultCode, Intent data){
+    super.onActivityResult(requestCode, resultCode, data);
+    restartApp();
+  }
 
-    private static void killCurrentProcess() {
-        android.os.Process.killProcess(android.os.Process.myPid());
-        System.exit(10);
-    }
+  private static void killCurrentProcess() {
+    android.os.Process.killProcess(android.os.Process.myPid());
+    System.exit(10);
+  }
 }
