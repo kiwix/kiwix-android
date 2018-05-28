cc17fedb19c2f5fdb76178f47e1bb04f882c6fef
siddharth2010 <ssiddharth2010@gmail.com>
2018-05-28 16:02:45 +0530

    add Tests for NetworkUtilsTest

--- app/src/test/java/org/kiwix/kiwixmobile/utils/NetworkUtilsTest.java
+++ app/src/test/java/org/kiwix/kiwixmobile/utils/NetworkUtilsTest.java
@@ -22,6 +22,7 @@ import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.when;
 
 import android.content.Context;
+import java.util.UUID;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.kiwix.kiwixmobile.R;
@@ -42,32 +43,49 @@ public class NetworkUtilsTest {
   //test that the file name returned for a given url is correct
   @Test
   public void testingFilenameFromUrl() {
-    //test that the uuid matches the default UUID Regex.
-    //TODO: find a way to assert regex matching via JUnit and rewrite the test
+    // TODO: find a way to assert regex matching via JUnit and rewrite the test
+
     String defaultUUIDRegex = "^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$";
     Pattern pattern = Pattern.compile(defaultUUIDRegex);
-    Matcher matcher = pattern.matcher(NetworkUtils.getFileNameFromUrl(""));
 
-    if(matcher.matches()) {
-      assertEquals("default filename doesn't match UUID regex",0,0);
-    } else {
-      assertEquals("default filename doesn't match UUID regex",0,1);
-    }
-    //TODO: add more test cases
+    // Case : Empty string passed
+    // File Name is a random UUID
+    Matcher matcher = pattern.matcher(NetworkUtils.getFileNameFromUrl(""));
+    if(!matcher.matches()) assertEquals("filename doesn't match UUID regex (for empty string URL)", 0, 1);
+
+    // Case : When the URL contains no '?' character but has '/' characters
+    // File Name is the substring from the last '/' character till the end of the string
+    assertEquals("File Name from URL (no '?' character)", "q=kiwix+android", NetworkUtils.getFileNameFromUrl("https://github.com/search/q=kiwix+android"));
+    // Unless '/' is also the last character in the URL (in which case its a default UUID)
+    matcher = pattern.matcher(NetworkUtils.getFileNameFromUrl("https://github.com/search/q=kiwix+android/"));
+    if(!matcher.matches()) assertEquals("filename doesn't match UUID regex (for no '?' and '/' in end)", 0, 1);
+
+    // Case : Empty string between last '?' and preceding '/'
+    // File Name is a random UUID
+    matcher = pattern.matcher(NetworkUtils.getFileNameFromUrl("https://github.com/search/?q=kiwix+android"));
+    if(!matcher.matches()) assertEquals("filename doesn't match UUID regex (for consecutive '/?')", 0, 1);
+
+    // Case : standard case
+    // Here the Method should return the substring between the first '?' character and the nearest '/' character preceeding it
+    assertEquals("File Name from URL standard case", "search", NetworkUtils.getFileNameFromUrl("https://www.google.com/search?source=hp&ei=zs4LW6W1E5n6rQH65Z-wDQ&q=kiwix+android&oq=kiwix+android&gs_l=psy-ab.3...2590.6259.0.6504.14.12.0.0.0.0.263.485.2-2.2.0....0...1c.1.64.psy-ab..12.2.485.0..0j35i39k1.0.WSlGY7hWzTo"));
+    assertEquals("File Name from URL standard case", "Special:Search", NetworkUtils.getFileNameFromUrl("https://en.wikipedia.org/wiki/Special:Search?search=kiwix+android&go=Go&searchToken=3zrcxw8fltdcij99zvoh5c6wy"));
+    assertEquals("File Name from URL standard case", "search", NetworkUtils.getFileNameFromUrl("https://github.com/search?q=kiwix+android"));
   }
 
-  //Test the parsed URL
+  // Test the parsed URL
   @Test
   public void testingParsedUrl() {
-    when(context.getString(R.string.zim_nopic)).thenReturn(Integer.toString(R.string.zim_nopic));
-    when(context.getString(R.string.zim_novid)).thenReturn(Integer.toString(R.string.zim_novid));
-    when(context.getString(R.string.zim_simple)).thenReturn(Integer.toString(R.string.zim_simple));
+    when(context.getString(R.string.zim_nopic)).thenReturn("No Pictures");
+    when(context.getString(R.string.zim_novid)).thenReturn("No Videos");
+    when(context.getString(R.string.zim_simple)).thenReturn("Simple");
 
-    assertEquals("empty string", "", NetworkUtils.parseURL(context, ""));
-    assertEquals("empty string", "2131624179", NetworkUtils.parseURL(context, "http://ftpmirror.your.org/pub/kiwix/zim/wikipedia/wikipedia_af_all_nopic_2016-05.zim"));
-    assertEquals("empty string", "2131624180", NetworkUtils.parseURL(context, "http://www.mirrorservice.org/sites/download.kiwix.org/zim/wikipedia/wikipedia_af_all_novid_2016-05.zim"));
-    assertEquals("empty string", "2131624181", NetworkUtils.parseURL(context, "http://download.wikimedia.org/kiwix/zim/wikipedia/wikipedia_af_all_simple_2016-05.zim"));
-    assertEquals("empty string", "2131624179", NetworkUtils.parseURL(context, "http://mirror.netcologne.de/kiwix/zim/wikipedia/wikipedia_af_all_nopic_2016-05.zim"));
-    assertEquals("empty string", "2131624181", NetworkUtils.parseURL(context, "http://mirror3.kiwix.org/zim/wikipedia/wikipedia_af_all_simple_2016-05.zim"));
+    assertEquals("URL Parsing on empty string", "", NetworkUtils.parseURL(context, ""));
+
+    //Using the standard Kiwix Download URLs
+    assertEquals("URL Parsing", "No Pictures", NetworkUtils.parseURL(context, "http://ftpmirror.your.org/pub/kiwix/zim/wikipedia/wikipedia_af_all_nopic_2016-05.zim"));
+    assertEquals("URL Parsing", "No Videos", NetworkUtils.parseURL(context, "http://www.mirrorservice.org/sites/download.kiwix.org/zim/wikipedia/wikipedia_af_all_novid_2016-05.zim"));
+    assertEquals("URL Parsing", "Simple", NetworkUtils.parseURL(context, "http://download.wikimedia.org/kiwix/zim/wikipedia/wikipedia_af_all_simple_2016-05.zim"));
+    assertEquals("URL Parsing", "No Pictures", NetworkUtils.parseURL(context, "http://mirror.netcologne.de/kiwix/zim/wikipedia/wikipedia_af_all_nopic_2016-05.zim"));
+    assertEquals("URL Parsing", "Simple", NetworkUtils.parseURL(context, "http://mirror3.kiwix.org/zim/wikipedia/wikipedia_af_all_simple_2016-05.zim"));
   }
 }
