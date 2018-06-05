93d30c85eaef73045d6b09a3e457030d4b8a1ed5
siddharth2010 <ssiddharth2010@gmail.com>
2018-06-06 03:25:51 +0530

    Add TODOs for BookDatabaseTest

---
+++ app/src/androidTestKiwix/java/org/kiwix/kiwixmobile/data/local/dao/BookDatabaseTest.java
@@ -0,0 +1,35 @@
+/*
+ * Kiwix Android
+ * Copyright (C) 2018  Kiwix <android.kiwix.org>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.kiwix.kiwixmobile.data.local.dao;
+
+import android.support.test.runner.AndroidJUnit4;
+import org.junit.runner.RunWith;
+
+@RunWith(AndroidJUnit4.class)
+public class BookDatabaseTest {
+
+  //TODO : test books are saved in the Database after download
+
+  //TODO : test books are saved after downloading the list of available zim files
+
+  //TODO : test book is deleted from database on deleting a specific zim file
+
+  //TODO : test the getBooks() method
+
+}
