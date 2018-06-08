e2a7014121da87e4ba06add33bedf56afc877b4f
siddharth2010 <ssiddharth2010@gmail.com>
2018-06-09 02:05:22 +0530

    Add tests for utils/files/FileUtils.java

--- app/src/androidTestKiwix/java/org/kiwix/kiwixmobile/utils/files/FileUtilsTest.java
+++ app/src/androidTestKiwix/java/org/kiwix/kiwixmobile/utils/files/FileUtilsTest.java
@@ -25,6 +25,7 @@ import static org.mockito.Mockito.when;
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
 import java.io.File;
+import java.util.List;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -38,6 +39,7 @@ public class FileUtilsTest {
 
   private boolean mockInitialized = false;
   private Context context;
+  //private FileUtils fileUtils;
 
   @Before
   public void executeBefore(){
@@ -46,7 +48,36 @@ public class FileUtilsTest {
       mockInitialized = true;
     }
     context = InstrumentationRegistry.getTargetContext();
+    //fileUtils = new FileUtils();
   }
 
-  //TODO : Test getAllZimParts and getCurrentSize methods
+  @Test
+  public void testGetAllZimParts(){
+    String testId = "8ce5775a-10a9-bbf3-178a-9df69f23263c";
+    String fileName = context.getFilesDir().getAbsolutePath() + File.separator + testId;
+    List<File> files;
+
+    //Test-1 (filename ends with .zim)
+    Book book1 = new Book();
+    book1.file = new File(fileName + ".zim");
+    files = FileUtils.getAllZimParts(book1);
+    assertEquals(1, files.size());
+    assertEquals(book1.file.getPath() + ".part", files.get(0).getPath());
+
+    //Test-2 (filename ends with .zim.part)
+    Book book2 = new Book();
+    book2.file = new File(fileName + ".zim.part");
+    files = FileUtils.getAllZimParts(book2);
+    assertEquals(1, files.size());
+    assertEquals(book2.file.getPath() + ".part", files.get(0).getPath());
+
+    //Test-3 (filename ends with .zimXX)
+    Book book3 = new Book();
+    book3.file = new File(fileName + ".zimab");
+    files = FileUtils.getAllZimParts(book3);
+    assertEquals(0, files.size());
+
+    //TODO : find a way to stub the file.exist() method and add more tests
+  }
+
 }
