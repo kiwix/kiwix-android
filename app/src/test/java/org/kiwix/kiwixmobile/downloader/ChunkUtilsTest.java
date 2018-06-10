bcd1b1eb8de62417b4f7443f0adb1c1450c9677a
siddharth2010 <ssiddharth2010@gmail.com>
2018-06-10 16:26:42 +0530

    Add tests for downloader/ChunkUtils

---
+++ app/src/test/java/org/kiwix/kiwixmobile/downloader/ChunkUtilsTest.java
@@ -0,0 +1,113 @@
+/*
+ * Kiwix Android
+ * Copyright (C) 2018  Kiwix <android.kiwix.org>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.kiwix.kiwixmobile.downloader;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.booleanThat;
+import static org.mockito.Mockito.when;
+
+import java.util.List;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.kiwix.kiwixmobile.downloader.ChunkUtils;
+
+@RunWith(MockitoJUnitRunner.class)
+public class ChunkUtilsTest {
+
+  @Test
+  public void TestGetChunks() {
+    List<Chunk> listReturned;
+    long size;
+    String URL = "http://mirror.netcologne.de/kiwix/zim/wikipedia/wikipedia_af_all_nopic_2016-05.zim";
+
+    // When the file size is exactly equal to CHUNK_SIZE
+    size = ChunkUtils.CHUNK_SIZE;
+    listReturned = ChunkUtils.getChunks(URL, size, 27);
+
+    assertEquals("verify that the list contains correct number of chunks", 1, listReturned.size());
+    assertEquals("verify that the range format is correct", "0-", listReturned.get(0).getRangeHeader());
+    assertEquals("verify that the same notificationID is passed to the chunk", 27, listReturned.get(0).getNotificationID());
+    assertEquals("verify that the file name ending is correctly assigned in case of a single file",
+        ".part", listReturned.get(0).getFileName().substring(listReturned.get(0).getFileName().length() - 5));
+    assertEquals("verify that the same URL is passed on to the chunk", URL, listReturned.get(0).getUrl());
+
+    // When the file size is more than CHUNK_SIZE
+    size = (ChunkUtils.CHUNK_SIZE * (long) 5) + (long) (1024 * 1024);
+    listReturned = ChunkUtils.getChunks(URL, size, 56);
+
+    assertEquals("verify that the list contains correct number of chunks", 6, listReturned.size());
+
+    assertEquals("verify that the rangehandler for the last chunk is correct",
+        "10737418245-", listReturned.get(listReturned.size() - 1).getRangeHeader());
+
+    assertEquals("verify that the rangehandler for the first chunk is corect",
+        "0-2147483648", listReturned.get(0).getRangeHeader());
+
+    assertEquals("verify that the same notificationID is passed on to each chunk",
+        true, listReturned.get(0).getUrl() == URL
+        && listReturned.get(1).getUrl() == URL
+        && listReturned.get(2).getUrl() == URL
+        && listReturned.get(3).getUrl() == URL
+        && listReturned.get(4).getUrl() == URL
+        && listReturned.get(5).getUrl() == URL);
+
+    assertEquals("verify that the same URL is passed on to each chunk",
+        true, listReturned.get(0).getNotificationID() == 56
+            && listReturned.get(1).getNotificationID() == 56
+            && listReturned.get(2).getNotificationID() == 56
+            && listReturned.get(3).getNotificationID() == 56
+            && listReturned.get(4).getNotificationID() == 56
+            && listReturned.get(5).getNotificationID() == 56);
+
+    // verify that previous extension in the filename (if any) is removed"
+    assertEquals("verify that previous extension in the filename (if any) is removed", "", "");
+
+    // test assignment of file names
+    boolean test = true;
+    String ALPHABET = "abcdefghijklmnopqrstuvwxyz";
+    for (int i = 0; i < listReturned.size(); i++) {
+      if (!listReturned.get(i)
+          .getFileName()
+          .substring(listReturned.get(i).getFileName().length() - 11)
+          .equals(".zim" + ALPHABET.charAt(i / 26) + ALPHABET.charAt(i % 26) + ".part")) {
+        test = false;
+      }
+    }
+    assertEquals("verify that the file name endings are correctly assigned", true, test);
+
+    // When the file size is less than CHUNK_SIZE
+    size = ChunkUtils.CHUNK_SIZE - (long) (1024 * 1024);
+    listReturned = ChunkUtils.getChunks(URL, size, 37);
+
+    assertEquals("verify that the list contains correct number of chunks", 1, listReturned.size());
+    assertEquals("verify that the range format is correct", "0-", listReturned.get(0).getRangeHeader());
+    assertEquals("verify that the same notificationID is passed to the chunk", 37, listReturned.get(0).getNotificationID());
+    assertEquals("verify that the file name ending is correctly assigned in case of a single file",
+        ".part", listReturned.get(0).getFileName().substring(listReturned.get(0).getFileName().length() - 5));
+    assertEquals("verify that the same URL is passed on to the chunk", URL, listReturned.get(0).getUrl());
+  }
+    //TODO : use powermock to mock the StorageUtils.getFileNamefromURL for the next test
+    // verify that filename is correctly generated"
+    //assertEquals("verify that previous extension in the filename (if any) is removed in case of files having more than 1 chunk", "", "");
+    //assertEquals("verify that previous extension in the filename (if any) is NOT removed in case of files having 1 chunk", "", "");
+
+}
