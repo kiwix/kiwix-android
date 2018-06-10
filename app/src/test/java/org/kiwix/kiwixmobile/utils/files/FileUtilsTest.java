689e98563b83cdde4742749f64ac375b7787edee
siddharth2010 <ssiddharth2010@gmail.com>
2018-06-10 23:36:12 +0530

    convert the FileUtils instrumentation test into a unit test

--- app/src/test/java/org/kiwix/kiwixmobile/utils/files/FileUtilsTest.java
+++ app/src/test/java/org/kiwix/kiwixmobile/utils/files/FileUtilsTest.java
@@ -19,67 +19,43 @@
 package org.kiwix.kiwixmobile.utils.files;
 
 import static org.junit.Assert.assertEquals;
-import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.when;
 
-import android.content.Context;
-import android.support.test.InstrumentationRegistry;
 import java.io.File;
 import java.util.List;
-import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.kiwix.kiwixmobile.library.entity.LibraryNetworkEntity.Book;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
-import org.mockito.junit.MockitoJUnitRunner;
 
-@RunWith(MockitoJUnitRunner.class)
 public class FileUtilsTest {
-
-  private boolean mockInitialized = false;
-  private Context context;
-  //private FileUtils fileUtils;
-
-  @Before
-  public void executeBefore(){
-    if (!mockInitialized) {
-      MockitoAnnotations.initMocks(this);
-      mockInitialized = true;
-    }
-    context = InstrumentationRegistry.getTargetContext();
-    //fileUtils = new FileUtils();
-  }
 
   //TODO : add more test cases and improve test
 
   @Test
   public void testGetAllZimParts(){
     String testId = "8ce5775a-10a9-bbf3-178a-9df69f23263c";
-    String fileName = context.getFilesDir().getAbsolutePath() + File.separator + testId;
+    String fileName = "/data/user/0/org.kiwix.kiwixmobile/files" + File.separator + testId;
     List<File> files;
 
-    //Test-1 (filename ends with .zim)
+    // Filename ends with .zim
     Book book1 = new Book();
     book1.file = new File(fileName + ".zim");
     files = FileUtils.getAllZimParts(book1);
-    assertEquals(1, files.size());
-    assertEquals(book1.file.getPath() + ".part", files.get(0).getPath());
+    assertEquals("Only a single book is returned in case the file has extension .zim", 1, files.size());
+    assertEquals("The filename is appended with .part", book1.file.getPath() + ".part", files.get(0).getPath());
 
-    //Test-2 (filename ends with .zim.part)
+    // Filename ends with .zim.part
     Book book2 = new Book();
     book2.file = new File(fileName + ".zim.part");
     files = FileUtils.getAllZimParts(book2);
-    assertEquals(1, files.size());
-    assertEquals(book2.file.getPath() + ".part", files.get(0).getPath());
+    assertEquals("Only a single book is returned in case the file has extension .zim.part", 1, files.size());
+    assertEquals("The filename is appended with .part", book2.file.getPath() + ".part", files.get(0).getPath());
 
-    //Test-3 (filename ends with .zimXX)
+    // Filename ends with .zimXX
     Book book3 = new Book();
     book3.file = new File(fileName + ".zimab");
     files = FileUtils.getAllZimParts(book3);
-    assertEquals(0, files.size());
+    assertEquals("Return no files in case no file exists at both 'path', and 'path.part'", 0, files.size());
 
-    //TODO : find a way to stub the file.exist() method and add more tests
+    // TODO : find a way to stub the file.exist() method and add more tests
   }
 
 }
