92ba2471fa94a01c187943290512c0d664d39899
siddharth2010 <ssiddharth2010@gmail.com>
2018-06-13 12:49:34 +0530

    create first draft of the instrumentation test

--- app/src/androidTestKiwix/java/org/kiwix/kiwixmobile/utils/files/FileUtilsInstrumentationTest.java
+++ app/src/androidTestKiwix/java/org/kiwix/kiwixmobile/utils/files/FileUtilsInstrumentationTest.java
@@ -9,6 +9,7 @@ import org.junit.Before;
 import org.junit.Test;
 import org.kiwix.kiwixmobile.data.local.KiwixDatabase;
 import org.kiwix.kiwixmobile.data.local.dao.BookDao;
+import org.kiwix.kiwixmobile.library.entity.LibraryNetworkEntity.Book;
 import org.mockito.MockitoAnnotations;
 
 import static org.junit.Assert.assertEquals;
@@ -17,6 +18,7 @@ public class FileUtilsInstrumentationTest {
 
   private Context context;
   private boolean mockInitialized = false;
+  private File testDir;
 
   @Before
   public void executeBefore() {
@@ -25,26 +27,56 @@ public class FileUtilsInstrumentationTest {
       mockInitialized = true;
     }
     context = InstrumentationRegistry.getTargetContext();
-    File testDir = context.getDir("testDir", context.MODE_PRIVATE);
-    //assertEquals("file path", "", testDir.getPath());
-    File newTestFile = new File(testDir.getPath() + "/newTestFile");
-    try {
-      newTestFile.createNewFile();
-    } catch (IOException e) {
-      e.printStackTrace();
-    }
-    assertEquals("assert file exists", true, newTestFile.exists());
+    testDir = context.getDir("testDir", context.MODE_PRIVATE);
 
-    File newTestFile2 = new File(newTestFile.getPath());
-    assertEquals("assert file exists", true, newTestFile2.exists());
+    //File newTestFile2 = new File(newTestFile.getPath());
+    //assertEquals("assert file exists", true, newTestFile2.exists());
   }
 
   @Test
-  public void testGetAllZimParts(){
+  public void testGetAllZimParts() throws IOException{
     // set Constants
     String testId = "8ce5775a-10a9-bbf3-178a-9df69f23263c";
-    String fileName = "/data/user/0/org.kiwix.kiwixmobile/files" + File.separator + testId;
-    List<File> files;
+
+    //assertEquals("file path", "", testDir.getPath());
+    //File testFile = new File(testDir.getPath() + "/newTestFile");
+    //try {
+    //  testFile.createNewFile();
+    //} catch (IOException e) {
+    //  e.printStackTrace();
+    //}
+    //assertEquals("verify that the file was created properly", true, testFile.exists());
+
+    // Filename ends with .zimXX and the files up till "FileName.zimer" exist
+    // i.e. 26 * 4 + 18 = 122 files exist
+    String fileName = "/" + testId + "testfile.zim";
+
+    // Creating the files for the test
+    for (char char1 = 'a'; char1 <= 'z'; char1++) {
+      for (char char2 = 'a'; char2 <= 'z'; char2++) {
+        File file = new File(testDir.getPath() + fileName + char1 + char2);
+        file.createNewFile();
+        if(char1 == 'e' && char2 == 'r') {
+          break;
+        }
+      }
+      if(char1 == 'e') {
+        break;
+      }
+    }
+
+    // Test begins here
+    Book book = new Book();
+    book.file = new File(testDir.getPath() + fileName + "bg");
+    assertEquals("verify that the file has been created properly", true, book.file.exists());
+    List<File> files = FileUtils.getAllZimParts(book);
+
+    // Checking all the values returned
+    assertEquals("26 * 4 + 18 = 122 files should be returned", 122, files.size());
+
+
+    // Deleting the Files
+
 
   }
 }
