f5995ea7b8e34abf6ea7b39263efba8ff6d8acf9
siddharth2010 <ssiddharth2010@gmail.com>
2018-06-14 01:41:40 +0530

    Files not being deleted at the end of the test
    Also. the test works perfectly in the first attempt, but after that it continuously fails each and everytime. This is expected because of the files that were created and not deleted

--- app/src/androidTestKiwix/java/org/kiwix/kiwixmobile/utils/files/FileUtilsInstrumentationTest.java
+++ app/src/androidTestKiwix/java/org/kiwix/kiwixmobile/utils/files/FileUtilsInstrumentationTest.java
@@ -29,7 +29,7 @@ public class FileUtilsInstrumentationTest {
 
     // Filename ends with .zimXX and the files up till "FileName.zimer" exist
     // i.e. 26 * 4 + 18 = 122 files exist
-    String testId = "8ce5775a-10a9-bbf3-178a-9df69f23263c";
+    String testId = "8ce5775a-10a9-bbf3-178a-9df69f23263s";
     String fileName = "/" + testId + "testfile.zim";
     String fileNameWithExtension;
     Random r = new Random();
@@ -44,6 +44,7 @@ public class FileUtilsInstrumentationTest {
         fileNameWithExtension = bool[index] ? fileNameWithExtension : fileNameWithExtension + ".part";
         File file = new File(testDir.getPath() + fileNameWithExtension);
         file.createNewFile();
+        file.deleteOnExit();
         if(char1 == 'e' && char2 == 'r') {
           break;
         }
@@ -73,6 +74,22 @@ public class FileUtilsInstrumentationTest {
       }
     }
 
+    //// Delete all the Files just created
+    //for (char char1 = 'a'; char1 <= 'z'; char1++) {
+    //  for (char char2 = 'a'; char2 <= 'z'; char2++) {
+    //    fileNameWithExtension = fileName + char1 + char2;
+    //    fileNameWithExtension = bool[index] ? fileNameWithExtension : fileNameWithExtension + ".part";
+    //    File file = new File(testDir.getPath() + fileNameWithExtension);
+    //    file.deleteOnExit();
+    //    if(char1 == 'e' && char2 == 'r') {
+    //      break;
+    //    }
+    //    index++;
+    //  }
+    //  if(char1 == 'e') {
+    //    break;
+    //  }
+    //}
   }
 
   @Test
@@ -83,30 +100,36 @@ public class FileUtilsInstrumentationTest {
     // FileName ends with .zim
     File file1 = new File(testDir + baseName + "1" + ".zim");
     file1.createNewFile();
+    file1.deleteOnExit();
     assertEquals("if the fileName ends with .zim and exists in memory, return false",
         false, FileUtils.hasPart(file1));
 
     // FileName ends with .part
     File file2 = new File(testDir + baseName + "2" + ".zim");
     file2.createNewFile();
+    file2.deleteOnExit();
     assertEquals("if the fileName ends with .part and exists in memory, return true",
         false, FileUtils.hasPart(file2));
 
     // FileName ends with .zim, however, only the FileName.zim.part file exists in memory
     File file3 = new File(testDir + baseName + "3" + ".zim" + ".part");
     file3.createNewFile();
+    file3.deleteOnExit();
     File file4 = new File(testDir + baseName + "3" + ".zim");
     assertEquals("if the fileName ends with .zim, but instead the .zim.part file exists in memory",
         true, FileUtils.hasPart(file4));
 
     // FileName ends with .zimXX
     File testCall = new File(testDir + baseName + ".zimcj");
+    testCall.createNewFile();
+    testCall.deleteOnExit();
 
-    // Case : FileName.zimXX does not exist for any value of "XX" from "aa" till "bl", but FileName.zimXX.part exists for all "XX" from "aa', till "bk", then it does not exist
+    // Case : FileName.zimXX.part does not exist for any value of "XX" from "aa" till "bl", but FileName.zimXX exists for all "XX" from "aa', till "bk", then it does not exist
     for (char char1 = 'a'; char1 <= 'z'; char1++) {
       for (char char2 = 'a'; char2 <= 'z'; char2++) {
-        File file = new File(testDir.getPath() + baseName + ".zim" + char1 + char2 + ".part");
+        File file = new File(testDir.getPath() + baseName + ".zim" + char1 + char2);
         file.createNewFile();
+        file.deleteOnExit();
         if(char1 == 'b' && char2 == 'k') {
           break;
         }
@@ -116,12 +139,19 @@ public class FileUtilsInstrumentationTest {
       }
     }
 
-    assertEquals("", false, FileUtils.hasPart(testCall));
+    //assertEquals("", false, FileUtils.hasPart(testCall));
+    //
+    //// Case : FileName.zim is the calling file, but neither FileName.zim, nor FileName.zim.part exist. In this case the answer will be the same as that in the previous (FileName.zimXX) case
+    //assertEquals("", true, FileUtils.hasPart(testCall));
 
-    // Case : FileName.zimXX exists for some "XX" between "aa" till "bl", and FileName.zimXX.part exists for all "XX" from "aa', till "bk", then it does not exist
-    File file = new File(testDir + baseName + ".zimav");
+    // Case : FileName.zimXX.part exists for some "XX" between "aa" till "bl", and FileName.zimXX exists for all "XX" from "aa', till "bk", then it does not exist
+    File file = new File(testDir + baseName + ".zimaa.part");
     file.createNewFile();
+    file.deleteOnExit();
     assertEquals("", true, FileUtils.hasPart(testCall));
-    
+
+    // Case : FileName.zim is the calling file, but neither FileName.zim, nor FileName.zim.part exist. In this case the answer will be the same as that in the previous (FileName.zimXX) case
+    assertEquals("", true, FileUtils.hasPart(testCall));
+
   }
 }
