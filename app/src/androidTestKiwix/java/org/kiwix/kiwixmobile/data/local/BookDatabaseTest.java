d9ab356898864dbc4a42bc8a721380d527b7a023
siddharth2010 <ssiddharth2010@gmail.com>
2018-06-15 04:18:05 +0530

    update tests

--- app/src/androidTestKiwix/java/org/kiwix/kiwixmobile/data/local/BookDatabaseTest.java
+++ app/src/androidTestKiwix/java/org/kiwix/kiwixmobile/data/local/BookDatabaseTest.java
@@ -71,9 +71,7 @@ public class BookDatabaseTest {
     testDir = context.getDir("testDir", context.MODE_PRIVATE);
   }
 
-  //TODO : test books are saved in the Database after download
   //TODO : test books are saved after downloading the list of available zim files
-  //TODO : test book is deleted from database on deleting a specific zim file
 
   @Test
   public void testGetBooks() throws IOException {
@@ -107,37 +105,88 @@ public class BookDatabaseTest {
     verify(kiwixDatabase).deleteWhere(BookDatabaseEntity.class, BookDatabaseEntity.URL.eq(booksToAdd.get(3).file.getPath()));
 
     // Filename ends with .zim and both the .zim, and the .zim.part files exists in memory
-    if(booksRetrieved.contains(booksToAdd.get(3))) assertEquals(0, 1);
+    if(!booksRetrieved.contains(booksToAdd.get(4))) assertEquals(0, 1);
+    verify(kiwixDatabase, never()).deleteWhere(BookDatabaseEntity.class, BookDatabaseEntity.URL.eq(booksToAdd.get(4).file.getPath()));
 
-    // If the filename ends with .zimXX, then the file is not included unless the file exists and ..
-    if(!booksRetrieved.contains(booksToAdd.get(4))) assertEquals(".zimXX",
-        0, 1);
-    if(!booksRetrieved.contains(booksToAdd.get(5))) assertEquals(".zimXX",
-        0, 1);
+    // If the filename ends with .zimXX
+
+    // FileName.zimXX.part does not exist for any value of "XX" from "aa" till "dr", but FileName.zimXX exists for all "XX" from "aa', till "ds", then it does not exist
+    // Also, the file inside the BooksToAdd does exist in memory
+    if(!booksRetrieved.contains(booksToAdd.get(5))) assertEquals(0, 1);
+    verify(kiwixDatabase, never()).deleteWhere(BookDatabaseEntity.class, BookDatabaseEntity.URL.eq(booksToAdd.get(5).file.getPath()));
+
+    // FileName.zimXX.part does not exist for any value of "XX" from "aa" till "dr", but FileName.zimXX exists for all "XX" from "aa', till "ds", then it does not exist
+    // Also, the file inside the BooksToAdd also not exist in memory
+    if(booksRetrieved.contains(booksToAdd.get(6))) assertEquals(0, 1);
+    verify(kiwixDatabase).deleteWhere(BookDatabaseEntity.class, BookDatabaseEntity.URL.eq(booksToAdd.get(6).file.getPath()));
+
+    // FileName.zimXX.part exists for some "XX" between "aa" till "bl"
+    // And FileName.zimXX exists for all "XX" from "aa', till "bk", and then it does not exist
+    // Also, the file inside the BooksToAdd does exist in memory
+    if(!booksRetrieved.contains(booksToAdd.get(7))) assertEquals(0, 1);
+    verify(kiwixDatabase, never()).deleteWhere(BookDatabaseEntity.class, BookDatabaseEntity.URL.eq(booksToAdd.get(7).file.getPath()));
+
+    // FileName.zimXX.part exists for some "XX" between "aa" till "bl"
+    // And FileName.zimXX exists for all "XX" from "aa', till "bk", and then it does not exist
+    // Also, the file inside the BooksToAdd does not exist in memory
+    if(booksRetrieved.contains(booksToAdd.get(8))) assertEquals(0, 1);
+    verify(kiwixDatabase).deleteWhere(BookDatabaseEntity.class, BookDatabaseEntity.URL.eq(booksToAdd.get(8).file.getPath()));
   }
 
   private ArrayList<Book> getFakeData(String baseFileName) throws IOException {
     ArrayList<Book> books = new ArrayList<>();
-    for(int i = 0; i < 7; i++){
+    for(int i = 0; i < 9; i++){
       Book book = new Book();
       book.bookName = "Test Copy " + Integer.toString(i);
       book.id = "Test ID " + Integer.toString(i);
       String fileName = baseFileName + Integer.toString(i);
       switch (i) {
-        case 0: book.file = new File(fileName + Integer.toString(i) + ".zim"); book.file.createNewFile(); break;
-        case 1: book.file = new File(fileName + Integer.toString(i) + ".part"); book.file.createNewFile(); break;
-        case 2: book.file = new File(fileName + Integer.toString(i) + ".zim");
-                File t2 = new File(fileName + Integer.toString(i) + ".zim.part"); t2.createNewFile(); break;
-        case 3: book.file = new File(fileName + Integer.toString(i) + ".zim"); break;
-        case 4: book.file = new File(fileName + Integer.toString(i) + ".zim"); book.file.createNewFile();
-                File t4 = new File(fileName + Integer.toString(i) + ".zim.part"); t4.createNewFile(); break;
-        case 5: book.file = new File(fileName + Integer.toString(i) + ".zimcj"); book.file.createNewFile(); break;
-        case 6: book.file = new File(fileName + Integer.toString(i) + ".zimcj");
-                File t6 = new File(fileName + Integer.toString(i) + ".zimcj.part"); t6.createNewFile(); break;
+        case 0: book.file = new File(fileName + ".zim"); book.file.createNewFile(); break;
+        case 1: book.file = new File(fileName + ".part"); book.file.createNewFile(); break;
+        case 2: book.file = new File(fileName + ".zim");
+                File t2 = new File(fileName + ".zim.part"); t2.createNewFile(); break;
+        case 3: book.file = new File(fileName + ".zim"); break;
+        case 4: book.file = new File(fileName + ".zim"); book.file.createNewFile();
+                File t4 = new File(fileName + ".zim.part"); t4.createNewFile(); break;
+        case 5: book.file = new File(fileName + ".zimdg"); setupCase1(fileName); break;
+        case 6: book.file = new File(fileName + ".zimyr"); setupCase2(fileName); break;
+        case 7: book.file = new File(fileName + ".zimdg"); setupCase1(fileName); break;
+        case 8: book.file = new File(fileName + ".zimyr"); setupCase2(fileName); break;
       }
       books.add(book);
     }
     return books;
+  }
+
+  private void setupCase1(String fileName) throws IOException{
+    for (char char1 = 'a'; char1 <= 'z'; char1++) {
+      for (char char2 = 'a'; char2 <= 'z'; char2++) {
+        File file = new File(fileName + ".zim" + char1 + char2);
+        file.createNewFile();
+        if (char1 == 'd' && char2 == 'r') {
+          break;
+        }
+      }
+      if (char1 == 'd') {
+        break;
+      }
+    }
+  }
+
+  private void setupCase2(String fileName) throws IOException{
+    for (char char1 = 'a'; char1 <= 'z'; char1++) {
+      for (char char2 = 'a'; char2 <= 'z'; char2++) {
+        File file = new File(fileName + ".zim" + char1 + char2);
+        file.createNewFile();
+        if (char1 == 'd' && char2 == 'r') {
+          break;
+        }
+      }
+      if (char1 == 'd') {
+        break;
+      }
+    }
+    File t = new File(fileName + ".zimcp.part");
   }
 
   @After
