d1e91dca4a6b4a760b925b2dc3a05de8e6a6c809
siddharth2010 <ssiddharth2010@gmail.com>
2018-06-29 01:14:39 +0530

    fixed all issues

--- app/src/androidTestKiwix/java/org/kiwix/kiwixmobile/intro/IntroActivityTest.java
+++ app/src/androidTestKiwix/java/org/kiwix/kiwixmobile/intro/IntroActivityTest.java
@@ -1,112 +1,89 @@
-/*
- * Kiwix Android
- * Copyright (C) 2018  Kiwix <android.kiwix.org>
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program. If not, see <http://www.gnu.org/licenses/>.
- */
-
 package org.kiwix.kiwixmobile.intro;
 
 import android.Manifest;
-import android.os.SystemClock;
+import android.support.test.espresso.ViewInteraction;
 import android.support.test.espresso.intent.Intents;
-import android.support.test.rule.ActivityTestRule;
 import android.support.test.rule.GrantPermissionRule;
 import android.support.test.runner.AndroidJUnit4;
 import android.test.suitebuilder.annotation.LargeTest;
 
-import org.junit.Before;
+import com.schibsted.spain.barista.interaction.BaristaMenuClickInteractions;
+import com.schibsted.spain.barista.interaction.BaristaSleepInteractions;
+import com.schibsted.spain.barista.rule.BaristaRule;
+
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.kiwix.kiwixmobile.R;
 import org.kiwix.kiwixmobile.main.MainActivity;
-import org.kiwix.kiwixmobile.splash.SplashActivity;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.swipeLeft;
+import static android.support.test.espresso.action.ViewActions.swipeRight;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.intent.Intents.intended;
 import static android.support.test.espresso.intent.matcher.IntentMatchers.hasComponent;
-import static android.support.test.espresso.intent.matcher.IntentMatchers.hasExtra;
-import static android.support.test.espresso.intent.matcher.IntentMatchers.toPackage;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static com.schibsted.spain.barista.interaction.BaristaClickInteractions.clickOn;
+import static org.hamcrest.Matchers.allOf;
 import static org.hamcrest.core.IsNull.notNullValue;
-import static org.junit.Assert.assertEquals;
-
-import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static org.kiwix.kiwixmobile.testutils.TestUtils.TEST_PAUSE_MS;
+import static org.kiwix.kiwixmobile.testutils.TestUtils.getResourceString;
+import static org.kiwix.kiwixmobile.testutils.TestUtils.withContent;
+import static org.kiwix.kiwixmobile.utils.StandardActions.enterHelp;
+import static org.kiwix.kiwixmobile.utils.StandardActions.enterSettings;
 
 @LargeTest
 @RunWith(AndroidJUnit4.class)
 public class IntroActivityTest {
-
   @Rule
-  public ActivityTestRule<IntroActivity> activityTestRule = new ActivityTestRule<>(IntroActivity.class);
+  public BaristaRule<IntroActivity> activityTestRule = BaristaRule.create(IntroActivity.class);
   @Rule
   public GrantPermissionRule readPermissionRule = GrantPermissionRule.grant(Manifest.permission.READ_EXTERNAL_STORAGE);
   @Rule
   public GrantPermissionRule writePermissionRule = GrantPermissionRule.grant(Manifest.permission.WRITE_EXTERNAL_STORAGE);
 
-  //@Test
-  //public void SplashActivitySimple() {
-  //
-  //}
+  @Test
+  public void FinalSimple() {
 
-  //@Before
-  //public void setUp(){
-  //  //context = InstrumentationRegistry.getContext();
-  //  //activityTestRule.launchActivity(null);
-  //  //Intents.init();
-  //  //preferences = PreferenceManager.getDefaultSharedPreferences(context);
-  //}
+  }
 
   @Test
-  public void testIntentforMainActivity() {
+  public void navigateHelp() {
+    activityTestRule.launchActivity();
+    BaristaSleepInteractions.sleep(TEST_PAUSE_MS);
     Intents.init();
-
-    // verify that the cards are displayed properly
-    boolean found1, found2;
-    found1 = found2 = false;
-    int i;
-
-    i = 0;
-    while(!found1 && i < 3) {
-      try {
-        //onView(withId(R.id.get_started)).perform(swipeLeft());
-        SystemClock.sleep(500);
-        //onView(withId(R.id.get_started)).check(matches(withText("Save books offline")));
-        onView(withText("Save books offline")).check(matches(notNullValue()));
-        found1 = true;
-      } catch (Exception e) {
-        // Do Nothing
-      }
-      i++;
-    }
-    assertEquals("siddharth", true, found1);
-
 
     // Verify that the button is there
     onView(withId(R.id.get_started)).check(matches(notNullValue()));
     onView(withId(R.id.get_started)).check(matches(withText("Get started")));
     onView(withId(R.id.get_started)).perform(click());
 
-
-
     // Test the intent generated for MainActivity
     intended(hasComponent(MainActivity.class.getName()));
   }
 
+  @Test
+  public void test2(){
+    activityTestRule.launchActivity();
+    BaristaSleepInteractions.sleep(TEST_PAUSE_MS);
+    ViewInteraction customViewPager = onView(withId(R.id.view_pager));
+
+    // Verify that the sliding view is working properly
+    customViewPager.perform(swipeLeft());
+    onView(allOf(withId(R.id.heading), withText("Save books offline"), isDisplayed()))
+        .check(matches(notNullValue()));
+    onView(allOf(withId(R.id.subheading), withText("Download books and read wherever you are."), isDisplayed()))
+        .check(matches(notNullValue()));
+
+    BaristaSleepInteractions.sleep(TEST_PAUSE_MS);
+    customViewPager.perform(swipeRight());
+    onView(allOf(withId(R.id.heading), withText("Welcome to the family"), isDisplayed()))
+        .check(matches(notNullValue()));
+    onView(allOf(withId(R.id.subheading), withText("Human kind's knowledge, on your phone."), isDisplayed()))
+        .check(matches(notNullValue()));
+  }
 }
