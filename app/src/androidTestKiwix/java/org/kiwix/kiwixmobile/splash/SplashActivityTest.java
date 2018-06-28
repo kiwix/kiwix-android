5cb2191f143798df264f4a5a05dd38264dbc88f8
siddharth2010 <ssiddharth2010@gmail.com>
2018-06-29 02:46:42 +0530

    improve SplashActivity Tests

--- app/src/androidTestKiwix/java/org/kiwix/kiwixmobile/splash/SplashActivityTest.java
+++ app/src/androidTestKiwix/java/org/kiwix/kiwixmobile/splash/SplashActivityTest.java
@@ -11,12 +11,14 @@ import android.test.suitebuilder.annotation.LargeTest;
 
 import com.schibsted.spain.barista.interaction.BaristaSleepInteractions;
 import com.schibsted.spain.barista.rule.BaristaRule;
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.kiwix.kiwixmobile.intro.IntroActivity;
 import org.kiwix.kiwixmobile.main.MainActivity;
 
+import static org.junit.Assert.assertEquals;
 import static org.kiwix.kiwixmobile.utils.Constants.PREF_SHOW_INTRO;
 import static android.support.test.InstrumentationRegistry.getInstrumentation;
 import static android.support.test.espresso.intent.Intents.intended;
@@ -27,6 +29,8 @@ import static org.kiwix.kiwixmobile.testutils.TestUtils.TEST_PAUSE_MS;
 @RunWith(AndroidJUnit4.class)
 public class SplashActivityTest {
 
+  private Context context;
+
   @Rule
   public BaristaRule<SplashActivity> activityTestRule = BaristaRule.create(SplashActivity.class);
   @Rule
@@ -34,22 +38,35 @@ public class SplashActivityTest {
   @Rule
   public GrantPermissionRule writePermissionRule = GrantPermissionRule.grant(Manifest.permission.WRITE_EXTERNAL_STORAGE);
 
+  @Before
+  public void setUp(){
+    context = getInstrumentation().getTargetContext();
+  }
+
   @Test
   public void testFirstRun() {
     Intents.init();
     activityTestRule.launchActivity();
     BaristaSleepInteractions.sleep(TEST_PAUSE_MS);
+
+    // Verify that the SplashActivity is followed by IntroActivity
     intended(hasComponent(IntroActivity.class.getName()));
+
+    // Verify that the value of the "intro shown" boolean inside the SharedPreferences Database is not changed until the "Get started" button is pressed
+    SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(context);
+    assertEquals("verify that 'intro shown' boolean value is not changed in the ",
+        true, preferences.getBoolean(PREF_SHOW_INTRO, true));
   }
 
   @Test
   public void testNormalRun() {
-    Context context = getInstrumentation().getTargetContext();
     SharedPreferences.Editor preferencesEditor = PreferenceManager.getDefaultSharedPreferences(context).edit();
     preferencesEditor.putBoolean(PREF_SHOW_INTRO, false).apply();
 
     activityTestRule.launchActivity();
     BaristaSleepInteractions.sleep(TEST_PAUSE_MS);
+
+    // Verify that the SplashActivity is followed by MainActivity
     intended(hasComponent(MainActivity.class.getName()));
   }
 }
