988414d03aaef2c4a178c5a5ec107a80e7e5934c
siddharth2010 <ssiddharth2010@gmail.com>
2018-06-30 19:06:22 +0530

    version 1 working

--- app/src/androidTestKiwix/java/org/kiwix/kiwixmobile/main/BottomBarTest.java
+++ app/src/androidTestKiwix/java/org/kiwix/kiwixmobile/main/BottomBarTest.java
@@ -23,29 +23,45 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
+import android.support.test.espresso.ViewInteraction;
 import android.support.test.espresso.intent.Intents;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.rule.GrantPermissionRule;
 import android.support.test.runner.AndroidJUnit4;
 import android.test.suitebuilder.annotation.LargeTest;
 
+//import android.view.View;
+//import android.view.ViewGroup;
+//import android.view.ViewParent;
 import com.schibsted.spain.barista.interaction.BaristaSleepInteractions;
 import com.schibsted.spain.barista.rule.BaristaRule;
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.kiwix.kiwixmobile.intro.IntroActivity;
+import org.kiwix.kiwixmobile.R;
 import org.kiwix.kiwixmobile.main.MainActivity;
 
-import static org.junit.Assert.assertEquals;
-import static org.kiwix.kiwixmobile.utils.Constants.PREF_BOTTOM_TOOLBAR;
-import static org.kiwix.kiwixmobile.utils.Constants.PREF_SHOW_INTRO;
 import static android.support.test.InstrumentationRegistry.getInstrumentation;
-import static android.support.test.espresso.intent.Intents.intended;
-import static android.support.test.espresso.intent.matcher.IntentMatchers.hasComponent;
 import static org.kiwix.kiwixmobile.testutils.TestUtils.TEST_PAUSE_MS;
+import static org.kiwix.kiwixmobile.utils.Constants.PREF_BOTTOM_TOOLBAR;
+
+//import static android.support.test.espresso.Espresso.onView;
+//import static android.support.test.espresso.action.ViewActions.click;
+//import static android.support.test.espresso.contrib.RecyclerViewActions.actionOnItemAtPosition;
+//import static android.support.test.espresso.matcher.ViewMatchers.withId;
+//import static org.hamcrest.Matchers.allOf;
+//import static org.junit.Assert.assertEquals;
+//import static org.kiwix.kiwixmobile.utils.Constants.PREF_BOTTOM_TOOLBAR;
+//import static org.kiwix.kiwixmobile.utils.Constants.PREF_SHOW_INTRO;
+//import static android.support.test.InstrumentationRegistry.getInstrumentation;
+//import static android.support.test.espresso.intent.Intents.intended;
+//import static android.support.test.espresso.intent.matcher.IntentMatchers.hasComponent;
+//import static org.kiwix.kiwixmobile.testutils.TestUtils.TEST_PAUSE_MS;
 
 @LargeTest
 @RunWith(AndroidJUnit4.class)
@@ -74,6 +90,31 @@ public class BottomBarTest {
     activityTestRule.launchActivity();
     BaristaSleepInteractions.sleep(TEST_PAUSE_MS);
 
-
+    //ViewInteraction recyclerView = onView(
+    //    allOf(withId(R.id.recycler_view),
+    //        childAtPosition(
+    //            withId(R.id.get_content_card),
+    //            1)));
+    //recyclerView.perform(actionOnItemAtPosition(1, click()));
+    Intents.release();
   }
+
+  //private static Matcher<View> childAtPosition(
+  //    final Matcher<View> parentMatcher, final int position) {
+  //
+  //  return new TypeSafeMatcher<View>() {
+  //    @Override
+  //    public void describeTo(Description description) {
+  //      description.appendText("Child at position " + position + " in parent ");
+  //      parentMatcher.describeTo(description);
+  //    }
+  //
+  //    @Override
+  //    public boolean matchesSafely(View view) {
+  //      ViewParent parent = view.getParent();
+  //      return parent instanceof ViewGroup && parentMatcher.matches(parent)
+  //          && view.equals(((ViewGroup) parent).getChildAt(position));
+  //    }
+  //  };
+  //}
 }
