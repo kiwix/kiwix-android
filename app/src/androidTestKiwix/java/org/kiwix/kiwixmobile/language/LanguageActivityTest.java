e222a50117a41ead1f63700d1760b1cf0895d448
siddharth2010 <ssiddharth2010@gmail.com>
2018-07-11 17:20:07 +0530

    add basic test flow

--- app/src/androidTestKiwix/java/org/kiwix/kiwixmobile/language/LanguageActivityTest.java
+++ app/src/androidTestKiwix/java/org/kiwix/kiwixmobile/language/LanguageActivityTest.java
@@ -18,12 +18,44 @@
 
 package org.kiwix.kiwixmobile.language;
 
-public class LanguageActivityTest {
-  // TODO: make sure the it is only possible to open the activity after the network call is finished and book list is updated
-  // TODO: test that default language is based on device locale
-  // TODO: test selecting language updates UI
-  // TODO: make sure selecting new languages does not generate a new network request
-  // TODO: verify all the correct books are displayed and are displayed correctly
-  // TODO: test selecting no language is allowed
+import android.Manifest;
+import android.support.test.espresso.intent.Intents;
+import android.support.test.rule.GrantPermissionRule;
+import com.schibsted.spain.barista.interaction.BaristaSleepInteractions;
+import com.schibsted.spain.barista.rule.BaristaRule;
+import org.junit.Rule;
+import org.junit.Test;
+import org.kiwix.kiwixmobile.R;
+import org.kiwix.kiwixmobile.intro.IntroActivity;
 
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static org.kiwix.kiwixmobile.testutils.TestUtils.TEST_PAUSE_MS;
+
+public class LanguageActivityTest {
+
+  @Rule
+  public BaristaRule<IntroActivity> activityTestRule = BaristaRule.create(IntroActivity.class);
+  @Rule
+  public GrantPermissionRule readPermissionRule = GrantPermissionRule.grant(Manifest.permission.READ_EXTERNAL_STORAGE);
+  @Rule
+  public GrantPermissionRule writePermissionRule = GrantPermissionRule.grant(Manifest.permission.WRITE_EXTERNAL_STORAGE);
+
+  @Test
+  public void testIntroActivity() {
+    Intents.init();
+    activityTestRule.launchActivity();
+    BaristaSleepInteractions.sleep(TEST_PAUSE_MS);
+    onView(withId(R.id.get_started)).perform(click());
+    BaristaSleepInteractions.sleep(TEST_PAUSE_MS);
+
+
+    // TODO: make sure the it is only possible to open the activity after the network call is finished and book list is updated
+    // TODO: test that default language is based on device locale
+    // TODO: test selecting language updates UI
+    // TODO: make sure selecting new languages does not generate a new network request
+    // TODO: verify all the correct books are displayed and are displayed correctly
+    // TODO: test selecting no language is allowed
+  }
 }
