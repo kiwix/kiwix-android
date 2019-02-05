9c7552c18ea7f0f748816d55879ebeadf8654752
Abdul Wadood <abdulwd97@gmail.com>
2019-02-05 23:26:12 +0530

    Reformat whole project using Square's code style

--- app/src/test/java/org/kiwix/kiwixmobile/utils/NetworkUtilsTest.java
+++ app/src/test/java/org/kiwix/kiwixmobile/utils/NetworkUtilsTest.java
@@ -34,10 +34,7 @@ import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.never;
-import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
@@ -50,45 +47,36 @@ public class NetworkUtilsTest {
   @Mock private NetworkInfo networkInfo1;
   @Mock private NetworkInfo networkInfo2;
 
-  @Test
+  //Sets the Build SDK version
+  private static void SetSDKVersion(Field field, Object newValue) throws Exception {
+    field.setAccessible(true);
+    Field modifiersField = Field.class.getDeclaredField("modifiers");
+    modifiersField.setAccessible(true);
+    modifiersField.setInt(field, field.getModifiers() & ~Modifier.FINAL);
+    field.set(null, newValue);
+  }
+
   public void testNetworkAvailability() {
-    NetworkInfo[] networkInfos = { networkInfo1, networkInfo2 };
+    NetworkInfo[] info = { networkInfo1, networkInfo2 };
     when(context.getSystemService(Context.CONNECTIVITY_SERVICE)).thenReturn(connectivity);
-    when(connectivity.getAllNetworkInfo()).thenReturn(networkInfos);
+    when(connectivity.getAllNetworkInfo()).thenReturn(info);
 
     //one network is connected
     when(networkInfo1.getState()).thenReturn(NetworkInfo.State.CONNECTED);
     when(networkInfo2.getState()).thenReturn(NetworkInfo.State.DISCONNECTING);
-    assertEquals(true, NetworkUtils.isNetworkAvailable(context, true));
+    assertEquals("", true, NetworkUtils.isNetworkAvailable(context));
 
-    //one network is connecting, includeConnecting true
+    //one network is connecting
     when(networkInfo1.getState()).thenReturn(NetworkInfo.State.DISCONNECTING);
     when(networkInfo2.getState()).thenReturn(NetworkInfo.State.CONNECTING);
-    assertEquals(true, NetworkUtils.isNetworkAvailable(context, true));
-
-    //one network is connecting, includeConnecting false
-    when(networkInfo1.getState()).thenReturn(NetworkInfo.State.DISCONNECTING);
-    when(networkInfo2.getState()).thenReturn(NetworkInfo.State.CONNECTING);
-    assertEquals(false, NetworkUtils.isNetworkAvailable(context, false));
+    assertEquals("", true, NetworkUtils.isNetworkAvailable(context));
 
     //no network is available
     when(networkInfo1.getState()).thenReturn(NetworkInfo.State.DISCONNECTED);
     when(networkInfo2.getState()).thenReturn(NetworkInfo.State.DISCONNECTED);
-    assertEquals(false, NetworkUtils.isNetworkAvailable(context, true));
+    assertEquals("", false, NetworkUtils.isNetworkAvailable(context));
   }
 
-  @Test
-  public void test_isNetworkConnectionOK() {
-    when(networkInfo2.getState()).thenReturn(NetworkInfo.State.CONNECTING);
-    assertTrue(NetworkUtils.isNetworkConnectionOK(networkInfo2, true));
-    assertFalse(NetworkUtils.isNetworkConnectionOK(networkInfo2, false));
-
-    when(networkInfo2.getState()).thenReturn(NetworkInfo.State.CONNECTED);
-    assertTrue(NetworkUtils.isNetworkConnectionOK(networkInfo2, true));
-    assertTrue(NetworkUtils.isNetworkConnectionOK(networkInfo2, false));
-  }
-
-  @Test
   public void testWifiAvailability() {
     when(context.getSystemService(Context.CONNECTIVITY_SERVICE)).thenReturn(connectivity);
     when(connectivity.getActiveNetworkInfo()).thenReturn(networkInfo);
@@ -103,23 +91,15 @@ public class NetworkUtilsTest {
     //on Mobile Data
     when(networkInfo.getType()).thenReturn(ConnectivityManager.TYPE_MOBILE);
     assertEquals(false, NetworkUtils.isWiFi(context));
-    //verify that the correct methods are used according to the build SDK version
+    //verify that the correct methods are used according to build SDK version
     verify(connectivity).getActiveNetworkInfo();
     verify(networkInfo).getType();
     verify(connectivity, never()).getNetworkInfo(ConnectivityManager.TYPE_WIFI);
 
-    //on WIFI connected
+    //on WIFI
     when(networkInfo.getType()).thenReturn(ConnectivityManager.TYPE_WIFI);
-    when(networkInfo.isConnected()).thenReturn(Boolean.TRUE);
     assertEquals(true, NetworkUtils.isWiFi(context));
-    verify(connectivity, times(2)).getActiveNetworkInfo();
-    verify(connectivity, never()).getNetworkInfo(ConnectivityManager.TYPE_WIFI);
-
-    //on WIFI disconnected
-    when(networkInfo.getType()).thenReturn(ConnectivityManager.TYPE_WIFI);
-    when(networkInfo.isConnected()).thenReturn(Boolean.FALSE);
-    assertEquals(false, NetworkUtils.isWiFi(context));
-    verify(connectivity, times(3)).getActiveNetworkInfo();
+    verify(connectivity).getActiveNetworkInfo();
     verify(connectivity, never()).getNetworkInfo(ConnectivityManager.TYPE_WIFI);
 
     //SDK < 23
@@ -133,13 +113,13 @@ public class NetworkUtilsTest {
     when(connectivity.getNetworkInfo(ConnectivityManager.TYPE_WIFI)).thenReturn(networkInfo);
     when(networkInfo.isConnected()).thenReturn(true);
     assertEquals(true, NetworkUtils.isWiFi(context));
-    verify(connectivity, times(1)).getNetworkInfo(ConnectivityManager.TYPE_WIFI);
+    verify(connectivity).getNetworkInfo(ConnectivityManager.TYPE_WIFI);
 
     //WIFI disconnected
     when(connectivity.getNetworkInfo(ConnectivityManager.TYPE_WIFI)).thenReturn(networkInfo);
     when(networkInfo.isConnected()).thenReturn(false);
     assertEquals(false, NetworkUtils.isWiFi(context));
-    verify(connectivity, times(2)).getNetworkInfo(ConnectivityManager.TYPE_WIFI);
+    verify(connectivity).getNetworkInfo(ConnectivityManager.TYPE_WIFI);
   }
 
   @Test
@@ -157,17 +137,17 @@ public class NetworkUtilsTest {
 
     // URL contains no '?' character but has '/' characters
     assertEquals("File Name from URL (no '?' character)", "q=kiwix+android",
-      NetworkUtils.getFileNameFromUrl("https://github.com/search/q=kiwix+android"));
+        NetworkUtils.getFileNameFromUrl("https://github.com/search/q=kiwix+android"));
     // and ends with a '/' character
     matcher = pattern.matcher(
-      NetworkUtils.getFileNameFromUrl("https://github.com/search/q=kiwix+android/"));
+        NetworkUtils.getFileNameFromUrl("https://github.com/search/q=kiwix+android/"));
     if (!matcher.matches()) {
       assertEquals("filename doesn't match UUID regex (for no '?' and '/' in end)", 0, 1);
     }
 
     // Empty string between last '?' and preceding '/'
     matcher = pattern.matcher(
-      NetworkUtils.getFileNameFromUrl("https://github.com/search/?q=kiwix+android"));
+        NetworkUtils.getFileNameFromUrl("https://github.com/search/?q=kiwix+android"));
     if (!matcher.matches()) {
       assertEquals("filename doesn't match UUID regex (for consecutive '/?')", 0, 1);
     }
@@ -175,12 +155,12 @@ public class NetworkUtilsTest {
     // Standard case
     // Here the Method should return the substring between the first '?' character and the nearest '/' character preceeding it
     assertEquals("File Name from URL standard case", "search", NetworkUtils.getFileNameFromUrl(
-      "https://www.google.com/search?source=hp&ei=zs4LW6W1E5n6rQH65Z-wDQ&q=kiwix+android&oq=kiwix+android&gs_l=psy-ab.3...2590.6259.0.6504.14.12.0.0.0.0.263.485.2-2.2.0....0...1c.1.64.psy-ab..12.2.485.0..0j35i39k1.0.WSlGY7hWzTo"));
+        "https://www.google.com/search?source=hp&ei=zs4LW6W1E5n6rQH65Z-wDQ&q=kiwix+android&oq=kiwix+android&gs_l=psy-ab.3...2590.6259.0.6504.14.12.0.0.0.0.263.485.2-2.2.0....0...1c.1.64.psy-ab..12.2.485.0..0j35i39k1.0.WSlGY7hWzTo"));
     assertEquals("File Name from URL standard case", "Special:Search",
-      NetworkUtils.getFileNameFromUrl(
-        "https://en.wikipedia.org/wiki/Special:Search?search=kiwix+android&go=Go&searchToken=3zrcxw8fltdcij99zvoh5c6wy"));
+        NetworkUtils.getFileNameFromUrl(
+            "https://en.wikipedia.org/wiki/Special:Search?search=kiwix+android&go=Go&searchToken=3zrcxw8fltdcij99zvoh5c6wy"));
     assertEquals("File Name from URL standard case", "search",
-      NetworkUtils.getFileNameFromUrl("https://github.com/search?q=kiwix+android"));
+        NetworkUtils.getFileNameFromUrl("https://github.com/search?q=kiwix+android"));
   }
 
   @Test
@@ -193,23 +173,14 @@ public class NetworkUtilsTest {
 
     //Using the standard Kiwix Download URLs
     assertEquals("URL Parsing", "No Pictures", NetworkUtils.parseURL(context,
-      "http://ftpmirror.your.org/pub/kiwix/zim/wikipedia/wikipedia_af_all_nopic_2016-05.zim"));
+        "http://ftpmirror.your.org/pub/kiwix/zim/wikipedia/wikipedia_af_all_nopic_2016-05.zim"));
     assertEquals("URL Parsing", "No Videos", NetworkUtils.parseURL(context,
-      "http://www.mirrorservice.org/sites/download.kiwix.org/zim/wikipedia/wikipedia_af_all_novid_2016-05.zim"));
+        "http://www.mirrorservice.org/sites/download.kiwix.org/zim/wikipedia/wikipedia_af_all_novid_2016-05.zim"));
     assertEquals("URL Parsing", "Simple", NetworkUtils.parseURL(context,
-      "http://download.wikimedia.org/kiwix/zim/wikipedia/wikipedia_af_all_simple_2016-05.zim"));
+        "http://download.wikimedia.org/kiwix/zim/wikipedia/wikipedia_af_all_simple_2016-05.zim"));
     assertEquals("URL Parsing", "No Pictures", NetworkUtils.parseURL(context,
-      "http://mirror.netcologne.de/kiwix/zim/wikipedia/wikipedia_af_all_nopic_2016-05.zim"));
+        "http://mirror.netcologne.de/kiwix/zim/wikipedia/wikipedia_af_all_nopic_2016-05.zim"));
     assertEquals("URL Parsing", "Simple", NetworkUtils.parseURL(context,
-      "http://mirror3.kiwix.org/zim/wikipedia/wikipedia_af_all_simple_2016-05.zim"));
-  }
-
-  //Sets the Build SDK version
-  private static void SetSDKVersion(Field field, Object newValue) throws Exception {
-    field.setAccessible(true);
-    Field modifiersField = Field.class.getDeclaredField("modifiers");
-    modifiersField.setAccessible(true);
-    modifiersField.setInt(field, field.getModifiers() & ~Modifier.FINAL);
-    field.set(null, newValue);
+        "http://mirror3.kiwix.org/zim/wikipedia/wikipedia_af_all_simple_2016-05.zim"));
   }
 }
