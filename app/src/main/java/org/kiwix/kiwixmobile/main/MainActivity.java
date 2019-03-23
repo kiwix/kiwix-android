3ba3fe38d1b84f19f1ee086adb3ff31e08f72023
Shridhar <shridhar.goel@gmail.com>
2019-03-23 12:32:59 +0530

    Swipe down on toolbar to open tabs switcher

--- app/src/main/java/org/kiwix/kiwixmobile/main/MainActivity.java
+++ app/src/main/java/org/kiwix/kiwixmobile/main/MainActivity.java
@@ -20,6 +20,7 @@
 package org.kiwix.kiwixmobile.main;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.appwidget.AppWidgetManager;
 import android.content.ActivityNotFoundException;
@@ -322,6 +323,7 @@ public class MainActivity extends BaseActivity implements WebViewCallback,
     }
   }
 
+  @SuppressLint("ClickableViewAccessibility")
   @Override
   public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
@@ -333,6 +335,13 @@ public class MainActivity extends BaseActivity implements WebViewCallback,
     setContentView(R.layout.activity_main);
     setSupportActionBar(toolbar);
     actionBar = getSupportActionBar();
+
+    toolbar.setOnTouchListener(new OnSwipeTouchListener(this) {
+
+      public void onSwipeBottom() {
+        showTabSwitcher();
+      }
+    });
 
     tableDrawerRight =
         tableDrawerRightContainer.getHeaderView(0).findViewById(R.id.right_drawer_list);
