dd6143aa423154b76d7e3361260c6168dfb49086
soloturn <soloturn@gmail.com>
2019-06-10 00:44:35 +0200

    enable build.gradle for kotlin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,13 @@
 buildscript {
+  var kotlin_version: String by extra
+  kotlin_version = "1.3.31"
   repositories {
     google()
     jcenter()
   }
   dependencies {
     classpath("com.android.tools.build:gradle:3.4.1")
+    classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version")
 
     // NOTE: Do not place your application dependencies here; they belong
     // in the individual module build.gradle files
