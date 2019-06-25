f90ba10b75426755bc14c92385ad9cdbb5aff33e
Aditya-Sood <sood.aditya.08@gmail.com>
2019-06-26 00:17:39 +0530

    Add 'FileItem' class to represent files when being transferred
    Fields: File name and transfer status (to be sent, sending, sent)

---
+++ app/src/main/java/org/kiwix/kiwixmobile/zim_manager/local_file_transfer/FileItem.java
@@ -0,0 +1,31 @@
+package org.kiwix.kiwixmobile.zim_manager.local_file_transfer;
+
+public class FileItem {
+  public static short TO_BE_SENT = -1;
+  public static short SENDING = 0;
+  public static short SENT = +1;
+
+  private String fileName = "";
+  private short fileStatus = TO_BE_SENT;
+
+  public FileItem(String fileName, short fileStatus) {
+    this.fileName = fileName;
+    this.fileStatus = fileStatus;
+  }
+
+  public void setFileName(String fileName) {
+    this.fileName = fileName;
+  }
+
+  public void setFileStatus(short fileStatus) {
+    this.fileStatus = fileStatus;
+  }
+
+  public String getFileName() {
+    return fileName;
+  }
+
+  public short getFileStatus() {
+    return fileStatus;
+  }
+}
