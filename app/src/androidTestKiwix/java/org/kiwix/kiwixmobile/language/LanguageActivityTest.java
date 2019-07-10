ff0705647a858c2b1fdcb7be6674af3a7342fef3
Justin Biggs <jhbscholastic@gmail.com>
2019-07-10 09:09:02 -0500

    reverted all tests

--- app/src/androidTestKiwix/java/org/kiwix/kiwixmobile/language/LanguageActivityTest.java
+++ app/src/androidTestKiwix/java/org/kiwix/kiwixmobile/language/LanguageActivityTest.java
@@ -89,15 +89,15 @@ public class LanguageActivityTest {
         isDisplayed()));
 
     // Verify that the "Choose Language" and the "Search" buttons are present only in the "online" tab
-   // onView(withContentDescription("Search")).check(matches(notNullValue()));
+    onView(withContentDescription("Search")).check(matches(notNullValue()));
     // Test that the language selection screen does not open if the "Choose language" button is clicked, while the data is being loaded
-//    onView(withContentDescription("Choose a language")).check(matches(notNullValue()))
-//        .perform(click());
+    onView(withContentDescription("Choose a language")).check(matches(notNullValue()))
+        .perform(click());
 
-//    viewPager.perform(swipeRight());
+    viewPager.perform(swipeRight());
     onView(withContentDescription("Search")).check(doesNotExist());
     onView(withContentDescription("Choose a language")).check(doesNotExist());
-//    viewPager.perform(swipeLeft());
+    viewPager.perform(swipeLeft());
     viewPager.perform(swipeLeft());
     onView(withContentDescription("Search")).check(doesNotExist());
     onView(withContentDescription("Choose a language")).check(doesNotExist());
