45208f64a712fa647b82c5ef981e7a5acf235828
Adeel Zafar <adeelzafar619@gmail.com>
2019-07-28 15:19:21 +0500

    Fix Hotspot Ip address For Android pie

--- app/src/main/java/org/kiwix/kiwixmobile/webserver/WebServerHelper.java
+++ app/src/main/java/org/kiwix/kiwixmobile/webserver/WebServerHelper.java
@@ -141,12 +141,21 @@ public class WebServerHelper {
           }
         }
       }
+      if (ip.length() > 14) {
+        for (int i = 15, j = 12; i < 18; i++, j++) {
+          if ((ip.charAt(i) == '.')) {
+            ip = ip.substring(0, j + 1); //from first char to 12
+            break;
+          }
+        }
+      }
     } catch (SocketException e) {
       // TODO Auto-generated catch block
       e.printStackTrace();
       ip += "Something Wrong! " + e.toString() + "\n";
-    }
-    Log.v("DANG", "Returning : " + "http://" + ip);
+    };
+
+    Log.v("DANG", "Returning : " + "http://" + ip)
     return "http://" + ip;
   }
 
