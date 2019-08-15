ddb96c064cbbbbb3b5358e197acc778966c377da
Abdul Wadood <abdulwd97@gmail.com>
2019-08-15 23:38:09 +0200

    Fix #948 Force redraw of RecyclerView children so that tabs are properly oriented on screen rotation.

--- app/src/main/java/org/kiwix/kiwixmobile/utils/ImageUtils.java
+++ app/src/main/java/org/kiwix/kiwixmobile/utils/ImageUtils.java
@@ -1,39 +1,49 @@
 package org.kiwix.kiwixmobile.utils;
 
 import android.graphics.Bitmap;
-import android.util.Log;
+import android.media.ThumbnailUtils;
 import android.view.View;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 
 public class ImageUtils {
-  public static Bitmap getBitmapFromView(View view) {
-    view.clearFocus();
-    view.setPressed(false);
-
-    boolean willNotCache = view.willNotCacheDrawing();
-    view.setWillNotCacheDrawing(false);
-
-    /*
-     * Reset the drawing cache background color to fully transparent
-     * for the duration of this operation
-     */
-    int color = view.getDrawingCacheBackgroundColor();
-    view.setDrawingCacheBackgroundColor(0);
-
-    if (color != 0) {
-      view.destroyDrawingCache();
+  @Nullable
+  public static Bitmap getBitmapFromView(final @NonNull View viewToDrawFrom, int width,
+    int height) {
+    boolean wasDrawingCacheEnabled = viewToDrawFrom.isDrawingCacheEnabled();
+    if (!wasDrawingCacheEnabled) {
+      viewToDrawFrom.setDrawingCacheEnabled(true);
     }
-    view.buildDrawingCache();
-    Bitmap cacheBitmap = view.getDrawingCache();
-    if (cacheBitmap == null) {
-      Log.e("ImageUtils", "Failed getViewBitmap(" + view + ")");
-      return null;
+    if (width <= 0 || height <= 0) {
+      if (viewToDrawFrom.getWidth() <= 0 || viewToDrawFrom.getHeight() <= 0) {
+        viewToDrawFrom.measure(View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED),
+          View.MeasureSpec
+            .makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED));
+        width = viewToDrawFrom.getMeasuredWidth();
+        height = viewToDrawFrom.getMeasuredHeight();
+      }
+      if (width <= 0 || height <= 0) {
+        final Bitmap bmp = viewToDrawFrom.getDrawingCache();
+        final Bitmap bitmap = bmp == null ? null : Bitmap.createBitmap(bmp);
+        if (!wasDrawingCacheEnabled) {
+          viewToDrawFrom.setDrawingCacheEnabled(false);
+        }
+        return bitmap;
+      }
+      viewToDrawFrom.layout(0, 0, width, height);
+    } else {
+      viewToDrawFrom.measure(View.MeasureSpec.makeMeasureSpec(width, View.MeasureSpec.EXACTLY),
+        View.MeasureSpec
+          .makeMeasureSpec(height, View.MeasureSpec.EXACTLY));
+      viewToDrawFrom.layout(0, 0, viewToDrawFrom.getMeasuredWidth(),
+        viewToDrawFrom.getMeasuredHeight());
     }
-    Bitmap bitmap = Bitmap.createBitmap(cacheBitmap);
-
-    // Restore the view
-    view.destroyDrawingCache();
-    view.setWillNotCacheDrawing(willNotCache);
-    view.setDrawingCacheBackgroundColor(color);
+    final Bitmap drawingCache = viewToDrawFrom.getDrawingCache();
+    final Bitmap bmp = ThumbnailUtils.extractThumbnail(drawingCache, width, height);
+    final Bitmap bitmap = bmp == null || bmp != drawingCache ? bmp : Bitmap.createBitmap(bmp);
+    if (!wasDrawingCacheEnabled) {
+      viewToDrawFrom.setDrawingCacheEnabled(false);
+    }
 
     return bitmap;
   }
