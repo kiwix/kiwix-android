cdf1df822cf6411a0ea9cb2f32cc607c2a51f8cb
Justin Biggs <jhbscholastic@gmail.com>
2019-11-30 16:27:22 -0600

    1581 prt one

--- core/src/main/java/org/kiwix/kiwixmobile/core/CoreApp.java
+++ core/src/main/java/org/kiwix/kiwixmobile/core/CoreApp.java
@@ -26,9 +26,6 @@ import android.util.Log;
 import androidx.appcompat.app.AppCompatDelegate;
 import androidx.multidex.MultiDexApplication;
 import com.jakewharton.threetenabp.AndroidThreeTen;
-import dagger.android.AndroidInjector;
-import dagger.android.DispatchingAndroidInjector;
-import dagger.android.HasActivityInjector;
 import java.io.File;
 import java.io.IOException;
 import javax.inject.Inject;
@@ -37,7 +34,7 @@ import org.kiwix.kiwixmobile.core.di.components.CoreComponent;
 import org.kiwix.kiwixmobile.core.di.components.DaggerCoreComponent;
 import org.kiwix.kiwixmobile.core.downloader.DownloadMonitor;
 
-public abstract class CoreApp extends MultiDexApplication implements HasActivityInjector {
+public abstract class CoreApp extends MultiDexApplication  {
 
   private static CoreApp app;
   private static CoreComponent coreComponent;
@@ -46,8 +43,6 @@ public abstract class CoreApp extends MultiDexApplication implements HasActivity
     AppCompatDelegate.setCompatVectorFromResourcesEnabled(true);
   }
 
-  @Inject
-  DispatchingAndroidInjector<Activity> activityInjector;
   @Inject
   DownloadMonitor downloadMonitor;
   @Inject
@@ -156,8 +151,4 @@ public abstract class CoreApp extends MultiDexApplication implements HasActivity
     return Environment.MEDIA_MOUNTED.equals(state);
   }
 
-  @Override
-  public AndroidInjector<Activity> activityInjector() {
-    return activityInjector;
-  }
 }
