76b357dbfdc461e16d098e50c3463969f1f43776
Sean Mac Gillicuddy <seantheappdev@gmail.com>
2019-12-05 14:56:56 +0000

    #1564 Error toast by starting kiwix - don't show toast for urls ending in null

--- core/src/main/java/org/kiwix/kiwixmobile/core/main/CoreMainActivity.java
+++ core/src/main/java/org/kiwix/kiwixmobile/core/main/CoreMainActivity.java
@@ -1580,8 +1580,10 @@ public abstract class CoreMainActivity extends BaseActivity
 
   @Override
   public void webViewFailedLoading(String url) {
-    String error = String.format(getString(R.string.error_article_url_not_found), url);
-    Toast.makeText(this, error, Toast.LENGTH_SHORT).show();
+    if (url != null && !url.endsWith("null")) {
+      String error = getString(R.string.error_article_url_not_found, url);
+      Toast.makeText(this, error, Toast.LENGTH_SHORT).show();
+    }
   }
 
   @Override
