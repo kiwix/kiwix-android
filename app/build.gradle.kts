9b82c2dab5cbeec60bde7c8fa6f70054d5453496
Sean Mac Gillicuddy <seantheappdev@gmail.com>
2019-12-10 16:46:44 +0000

    update version number

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ apply(from = rootProject.file("jacoco.gradle"))
 
 ext {
   set("versionMajor", 3)
-  set("versionMinor", 0)
-  set("versionPatch", 4)
+  set("versionMinor", 1)
+  set("versionPatch", 0)
 }
 
 fun generateVersionName() = "${ext["versionMajor"]}.${ext["versionMinor"]}.${ext["versionPatch"]}"
