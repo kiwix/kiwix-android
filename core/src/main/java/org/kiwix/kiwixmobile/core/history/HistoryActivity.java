98f21c908270ca64440aec273cedc17c84d21483
Yash Khare <yashsja@gmail.com>
2020-01-29 18:25:22 +0530

    #1058 Revamp the menu of the History Activity

--- core/src/main/java/org/kiwix/kiwixmobile/core/history/HistoryActivity.java
+++ core/src/main/java/org/kiwix/kiwixmobile/core/history/HistoryActivity.java
@@ -26,7 +26,9 @@ import android.provider.Settings;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.CompoundButton;
 import android.widget.ImageView;
+import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
 import androidx.appcompat.app.ActionBar;
@@ -74,6 +76,8 @@ public class HistoryActivity extends BaseActivity implements HistoryContract.Vie
   RecyclerView recyclerView;
   @BindView(R2.id.no_history)
   TextView noHistory;
+  @BindView(R2.id.history_switch)
+  Switch historySwitch;
   private boolean refreshAdapter = true;
   private HistoryAdapter historyAdapter;
   private LinearLayoutManager layoutManager;
@@ -148,6 +152,15 @@ public class HistoryActivity extends BaseActivity implements HistoryContract.Vie
     recyclerView.setAdapter(historyAdapter);
     layoutManager = (LinearLayoutManager) recyclerView.getLayoutManager();
     recyclerView.setLayoutManager(layoutManager);
+
+    historySwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+      @Override public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+        sharedPreferenceUtil.setShowHistoryCurrentBook(!isChecked);
+        presenter.loadHistory(sharedPreferenceUtil.getShowHistoryCurrentBook());
+      }
+    });
+
+    historySwitch.setChecked(!sharedPreferenceUtil.getShowHistoryCurrentBook());
   }
 
   private void setupHistoryAdapter() {
@@ -186,30 +199,31 @@ public class HistoryActivity extends BaseActivity implements HistoryContract.Vie
 
   @Override
   public boolean onCreateOptionsMenu(Menu menu) {
-    getMenuInflater().inflate(R.menu.menu_history, menu);
-    MenuItem toggle = menu.findItem(R.id.menu_history_toggle);
-    toggle.setChecked(sharedPreferenceUtil.getShowHistoryCurrentBook());
 
-    SearchView search = (SearchView) menu.findItem(R.id.menu_history_search).getActionView();
-    search.setQueryHint(getString(R.string.search_history));
-    search.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
-      @Override
-      public boolean onQueryTextSubmit(String query) {
-        return false;
-      }
+    if (!historyList.isEmpty()) {
+      getMenuInflater().inflate(R.menu.menu_history, menu);
 
-      @Override
-      public boolean onQueryTextChange(String newText) {
-        historyList.clear();
-        historyList.addAll(fullHistory);
-        if ("".equals(newText)) {
-          historyAdapter.notifyDataSetChanged();
+      SearchView search = (SearchView) menu.findItem(R.id.menu_history_search).getActionView();
+      search.setQueryHint(getString(R.string.search_history));
+      search.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+        @Override
+        public boolean onQueryTextSubmit(String query) {
+          return false;
+        }
+
+        @Override
+        public boolean onQueryTextChange(String newText) {
+          historyList.clear();
+          historyList.addAll(fullHistory);
+          if ("".equals(newText)) {
+            historyAdapter.notifyDataSetChanged();
+            return true;
+          }
+          presenter.filterHistory(historyList, newText);
           return true;
         }
-        presenter.filterHistory(historyList, newText);
-        return true;
-      }
-    });
+      });
+    }
     return true;
   }
 
@@ -218,11 +232,6 @@ public class HistoryActivity extends BaseActivity implements HistoryContract.Vie
     int itemId = item.getItemId();
     if (itemId == android.R.id.home) {
       onBackPressed();
-      return true;
-    } else if (itemId == R.id.menu_history_toggle) {
-      item.setChecked(!item.isChecked());
-      sharedPreferenceUtil.setShowHistoryCurrentBook(item.isChecked());
-      presenter.loadHistory(sharedPreferenceUtil.getShowHistoryCurrentBook());
       return true;
     } else if (itemId == R.id.menu_history_clear) {
       presenter.deleteHistory(new ArrayList<>(fullHistory));
