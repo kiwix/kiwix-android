79cfa436adef2393d66b78f54171e6ab89e5003e
Saurav Tiwary <srv.twry@gmail.com>
2020-03-03 02:55:16 +0530

    Fixes #1843 Convert BookmarksContract to Kotlin
    Signed-off-by: Saurav Tiwary <srv.twry@gmail.com>

--- core/src/main/java/org/kiwix/kiwixmobile/core/bookmark/BookmarksContract.kt
+++ core/src/main/java/org/kiwix/kiwixmobile/core/bookmark/BookmarksContract.kt
@@ -15,24 +15,27 @@
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  *
  */
+package org.kiwix.kiwixmobile.core.bookmark
 
-package org.kiwix.kiwixmobile.core.bookmark;
+import org.kiwix.kiwixmobile.core.base.BaseContract
+import org.kiwix.kiwixmobile.core.base.BaseContract.Presenter
+import org.kiwix.kiwixmobile.core.base.BaseContract.View
 
-import java.util.List;
-import org.kiwix.kiwixmobile.core.base.BaseContract;
-
-interface BookmarksContract {
-  interface View extends BaseContract.View<Presenter> {
-    void updateBookmarksList(List<BookmarkItem> bookmarks);
-
-    void notifyBookmarksListFiltered(List<BookmarkItem> bookmarks);
+internal interface BookmarksContract {
+  interface View :
+    BaseContract.View<Presenter?> {
+    fun updateBookmarksList(bookmarks: List<BookmarkItem?>?)
+    fun notifyBookmarksListFiltered(bookmarks: List<BookmarkItem?>?)
   }
 
-  interface Presenter extends BaseContract.Presenter<View> {
-    void loadBookmarks(boolean showBookmarksCurrentBook);
+  interface Presenter :
+    BaseContract.Presenter<View?> {
+    fun loadBookmarks(showBookmarksCurrentBook: Boolean)
+    fun filterBookmarks(
+      bookmarksList: List<BookmarkItem?>?,
+      newText: String?
+    )
 
-    void filterBookmarks(List<BookmarkItem> bookmarksList, String newText);
-
-    void deleteBookmarks(List<BookmarkItem> deleteList);
+    fun deleteBookmarks(deleteList: List<BookmarkItem?>?)
   }
 }
