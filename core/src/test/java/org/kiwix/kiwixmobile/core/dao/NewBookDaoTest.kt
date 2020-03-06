56f24d1db962523fdcdc1db2b9239ce37b0965b3
Saurav Tiwary <srv.twry@gmail.com>
2020-03-07 01:43:41 +0530

    Init NewBookDaoTest

---
+++ core/src/test/java/org/kiwix/kiwixmobile/core/dao/NewBookDaoTest.kt
@@ -0,0 +1,64 @@
+/*
+ * Kiwix Android
+ * Copyright (c) 2020 Kiwix <android.kiwix.org>
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+package org.kiwix.kiwixmobile.core.dao
+
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+internal class NewBookDaoTest {
+
+  @BeforeEach
+  fun setUp() {
+  }
+
+  @AfterEach
+  fun tearDown() {
+  }
+
+  @Test
+  fun books() {
+  }
+
+  @Test
+  fun getBooks() {
+  }
+
+  @Test
+  fun insert() {
+  }
+
+  @Test
+  fun delete() {
+  }
+
+  @Test
+  fun migrationInsert() {
+  }
+
+  @Test
+  fun getFavIconAndZimFile() {
+  }
+
+  @Test
+  fun bookMatching() {
+  }
+}
