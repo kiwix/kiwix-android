24f840c8b5a2769003a508f2da1a67d5e2f15078
Yash Khare <yashsja@gmail.com>
2020-04-26 13:24:02 +0530

    test

--- contrib/instrumentation.sh
+++ contrib/instrumentation.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+adb shell screencap -p /sdcard/screencap.png
+adb pull /sdcard/screencap.png
+find screencap.png
 adb logcat -c
 adb logcat *:E -v color &
 ./gradlew jacocoInstrumentationTestReport
