0fd82771cc966d477af302eea684919b124a5061
Yash Khare <yashsja@gmail.com>
2020-04-28 15:35:40 +0530

    Fix naming

--- contrib/instrumentation.sh
+++ contrib/instrumentation.sh
@@ -3,7 +3,6 @@
 adb logcat -c
 adb logcat *:E -v color &
 ./gradlew jacocoInstrumentationTestReport
-adb remount
 adb shell screencap -p /sdcard/screencap.png
 adb pull /sdcard/screencap.png
 find screencap.png
