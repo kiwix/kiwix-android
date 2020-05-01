f1e9f9c8c959c1fa70ec20a3b8b18f09fed00595
Yash Khare <yashsja@gmail.com>
2020-05-01 12:30:55 +0530

    Fix for API 21 and 22

--- contrib/instrumentation.sh
+++ contrib/instrumentation.sh
@@ -3,11 +3,6 @@
 adb logcat -c
 adb logcat *:E -v color &
 ./gradlew jacocoInstrumentationTestReport
-adb shell screencap -p /sdcard/screencap.png
-adb shell
-run-as org.kiwix.kiwixmobile
-cp '/sdcard/screencap.png' '/mnt/sdcard/'
-exit
-exit
-adb pull /sdcard/screencap.png
+adb shell screencap -p /mnt/sdcard/screencap.png
+adb pull /mnt/sdcard/screencap.png
 find screencap.png
