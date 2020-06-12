3ce3f876e4864dbbbda206e6bbdf9d685c997d92
HissPirat <franslukas@gmail.com>
2020-06-12 15:55:10 +0200

    #2112 setting onSwipeTouchListener works

--- core/src/main/java/org/kiwix/kiwixmobile/core/main/CoreReaderFragmentSmall.java
+++ core/src/main/java/org/kiwix/kiwixmobile/core/main/CoreReaderFragmentSmall.java
@@ -18,23 +18,133 @@
 
 package org.kiwix.kiwixmobile.core.main;
 
+import android.annotation.SuppressLint;
+import android.graphics.Rect;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.AnimationUtils;
+import androidx.annotation.AnimRes;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import java.util.ArrayList;
+import java.util.List;
+import javax.inject.Inject;
+import org.jetbrains.annotations.NotNull;
 import org.kiwix.kiwixmobile.core.R;
+import org.kiwix.kiwixmobile.core.R2;
+import org.kiwix.kiwixmobile.core.extensions.ViewGroupExtensions;
+import org.kiwix.kiwixmobile.core.reader.ZimFileReader;
+import org.kiwix.kiwixmobile.core.reader.ZimReaderContainer;
 
 public class CoreReaderFragmentSmall extends Fragment {
 
+  @BindView(R2.id.toolbar)
+  Toolbar toolbar;
+
+  private MainMenu mainMenu;
+  protected int currentWebViewIndex = 0;
+  protected final List<KiwixWebView> webViewList = new ArrayList<>();
+
+  @Inject
+  protected ZimReaderContainer zimReaderContainer;
+
+  @SuppressLint("ClickableViewAccessibility")
   @Nullable @Override public View onCreateView(@NonNull LayoutInflater inflater,
     @Nullable ViewGroup container,
     @Nullable Bundle savedInstanceState) {
     View root = inflater.inflate(R.layout.fragment_main, container, false);
-    //Toolbar toolbar = root.findViewById(R.id.fragment_main_app_bar);
-    //((AppCompatActivity) getActivity()).setSupportActionBar(toolbar);
+    ButterKnife.bind(this, root);
+    ((AppCompatActivity) getActivity()).setSupportActionBar(toolbar);
+    toolbar.setOnTouchListener(new OnSwipeTouchListener(getActivity()) {
+
+      @Override
+      @SuppressLint("SyntheticAccessor")
+      public void onSwipeBottom() {
+        showTabSwitcher();
+      }
+
+      @Override
+      public void onSwipeLeft() {
+        if (currentWebViewIndex < webViewList.size() - 1) {
+          View current = getCurrentWebView();
+          startAnimation(current, R.anim.transition_left);
+          selectTab(currentWebViewIndex + 1);
+        }
+      }
+
+      @Override
+      public void onSwipeRight() {
+        if (currentWebViewIndex > 0) {
+          View current = getCurrentWebView();
+          startAnimation(current, R.anim.transition_right);
+          selectTab(currentWebViewIndex - 1);
+        }
+      }
+
+      @Override public void onTap(MotionEvent e) {
+        final View titleTextView = ViewGroupExtensions.findFirstTextView(toolbar);
+        if (titleTextView == null) return;
+        final Rect hitRect = new Rect();
+        titleTextView.getHitRect(hitRect);
+        if (hitRect.contains((int) e.getX(), (int) e.getY())) {
+          if (mainMenu != null) {
+            mainMenu.tryExpandSearch(zimReaderContainer.getZimFileReader());
+          }
+        }
+      }
+    });
+
     return root;
+  }
+
+  //TODO
+  private void showTabSwitcher() {
+
+  }
+
+  //TODO
+  protected void selectTab(int position) {
+
+  }
+
+  //TODO
+  protected KiwixWebView newTab(String url) {
+    return null;
+  }
+
+  @Override public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+    super.onViewCreated(view, savedInstanceState);
+    //((AppCompatActivity) getActivity()).setSupportActionBar(toolbar);
+  }
+
+  private void startAnimation(View view, @AnimRes int anim) {
+    view.startAnimation(AnimationUtils.loadAnimation(view.getContext(), anim));
+  }
+
+  protected KiwixWebView getCurrentWebView() {
+    if (webViewList.size() == 0) return newMainPageTab();
+    if (currentWebViewIndex < webViewList.size()) {
+      return webViewList.get(currentWebViewIndex);
+    } else {
+      return webViewList.get(0);
+    }
+  }
+
+  protected KiwixWebView newMainPageTab() {
+    return newTab(contentUrl(zimReaderContainer.getMainPage()));
+  }
+
+  @NotNull
+  private String contentUrl(String articleUrl) {
+    return Uri.parse(ZimFileReader.CONTENT_PREFIX + articleUrl).toString();
   }
 }
