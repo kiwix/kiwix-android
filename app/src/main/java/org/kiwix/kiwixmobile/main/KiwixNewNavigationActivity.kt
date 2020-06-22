bc532d7abf3459137204b20ac4534138831fd760
HissPirat <franslukas@gmail.com>
2020-06-22 13:22:12 +0200

    #2159 added necessary layouts, fragments and activity classes for navigation

--- app/src/main/java/org/kiwix/kiwixmobile/main/KiwixNewNavigationActivity.kt
+++ app/src/main/java/org/kiwix/kiwixmobile/main/KiwixNewNavigationActivity.kt
@@ -18,12 +18,82 @@
 
 package org.kiwix.kiwixmobile.main
 
+import android.content.Intent
+import android.os.Bundle
+import android.view.ActionMode
+import androidx.appcompat.widget.Toolbar
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import org.kiwix.kiwixmobile.R
+import org.kiwix.kiwixmobile.core.base.BaseFragmentActivityExtensions
 import org.kiwix.kiwixmobile.core.di.components.CoreComponent
 import org.kiwix.kiwixmobile.core.main.CoreNewNavigationActivity
 import org.kiwix.kiwixmobile.kiwixActivityComponent
 
-class KiwixNewNavigationActivity : CoreNewNavigationActivity {
+class KiwixNewNavigationActivity : CoreNewNavigationActivity() {
+  private lateinit var navController: NavController
+  private lateinit var appBarConfiguration: AppBarConfiguration
   override fun injection(coreComponent: CoreComponent) {
     kiwixActivityComponent.inject(this)
+  }
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_new_navigation)
+    val navView: BottomNavigationView = findViewById(R.id.nav_view)
+
+    navController = findNavController(R.id.nav_host_fragment)
+    // Passing each menu ID as a set of Ids because each
+    // menu should be considered as top level destinations.
+    appBarConfiguration = AppBarConfiguration(
+      navController.graph
+    )
+    val appBarConfiguration = AppBarConfiguration(
+      setOf(
+        R.id.navigation_reader,
+        R.id.navigation_library,
+        R.id.navigation_downloads
+      )
+    )
+    findViewById<Toolbar>(R.id.toolbar).setupWithNavController(navController, appBarConfiguration)
+    navView.setupWithNavController(navController)
+  }
+
+  override fun onSupportNavigateUp(): Boolean {
+    val navController = findNavController(R.id.nav_host_fragment)
+    return navController.navigateUp() ||
+      super.onSupportNavigateUp()
+  }
+
+  override fun onActionModeStarted(mode: ActionMode) {
+    super.onActionModeStarted(mode)
+    supportFragmentManager.fragments.filterIsInstance<BaseFragmentActivityExtensions>().forEach {
+      it.onActionModeStarted(mode, this)
+    }
+  }
+
+  override fun onActionModeFinished(mode: ActionMode) {
+    super.onActionModeFinished(mode)
+    supportFragmentManager.fragments.filterIsInstance<BaseFragmentActivityExtensions>().forEach {
+      it.onActionModeFinished(mode, this)
+    }
+  }
+
+  override fun onBackPressed() {
+    supportFragmentManager.fragments.filterIsInstance<BaseFragmentActivityExtensions>().forEach {
+      if (it.onBackPressed(this) == BaseFragmentActivityExtensions.Super.ShouldCall) {
+        super.onBackPressed()
+      }
+    }
+  }
+
+  override fun onNewIntent(intent: Intent) {
+    super.onNewIntent(intent)
+    supportFragmentManager.fragments.filterIsInstance<BaseFragmentActivityExtensions>().forEach {
+      it.onNewIntent(intent, this)
+    }
   }
 }
--- core/src/main/java/org/kiwix/kiwixmobile/core/main/CoreReaderFragment.java
+++ core/src/main/java/org/kiwix/kiwixmobile/core/main/CoreReaderFragment.java
@@ -108,7 +108,6 @@ import org.kiwix.kiwixmobile.core.extensions.ViewExtensionsKt;
 import org.kiwix.kiwixmobile.core.extensions.ViewGroupExtensions;
 import org.kiwix.kiwixmobile.core.page.bookmark.BookmarksActivity;
 import org.kiwix.kiwixmobile.core.page.bookmark.adapter.BookmarkItem;
-import org.kiwix.kiwixmobile.core.page.history.adapter.HistoryListItem;
 import org.kiwix.kiwixmobile.core.reader.ZimFileReader;
 import org.kiwix.kiwixmobile.core.reader.ZimReaderContainer;
 import org.kiwix.kiwixmobile.core.search.SearchActivity;
@@ -130,7 +129,7 @@ import static android.app.Activity.RESULT_OK;
 import static android.content.pm.PackageManager.PERMISSION_GRANTED;
 import static org.kiwix.kiwixmobile.core.downloader.fetch.FetchDownloadNotificationManagerKt.DOWNLOAD_NOTIFICATION_TITLE;
 import static org.kiwix.kiwixmobile.core.page.history.HistoryActivityKt.USER_CLEARED_HISTORY;
-import static org.kiwix.kiwixmobile.core.page.history.adapter.HistoryListItem.*;
+import static org.kiwix.kiwixmobile.core.page.history.adapter.HistoryListItem.HistoryItem;
 import static org.kiwix.kiwixmobile.core.utils.AnimationUtils.rotate;
 import static org.kiwix.kiwixmobile.core.utils.ConstantsKt.BOOKMARK_CHOSEN_REQUEST;
 import static org.kiwix.kiwixmobile.core.utils.ConstantsKt.EXTRA_CHOSE_X_FILE;
@@ -714,7 +713,7 @@ public abstract class CoreReaderFragment extends BaseFragment
   }
 
   private void updateTitle() {
-    actionBar.setTitle(getValidTitle(zimReaderContainer.getZimFileTitle()) + " (FRAGMENT)");
+    actionBar.setTitle(getValidTitle(zimReaderContainer.getZimFileTitle()));
   }
 
   private String getValidTitle(String zimFileTitle) {
