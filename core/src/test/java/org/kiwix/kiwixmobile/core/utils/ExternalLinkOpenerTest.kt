d8bc38c714f8e2acc51914400a8da30e2a095ca5
HissPirat <franslukas@gmail.com>
2020-07-29 15:19:32 +0200

    #2227 added tests for ExternalLinkOpener

--- core/src/test/java/org/kiwix/kiwixmobile/core/utils/ExternalLinkOpenerTest.kt
+++ core/src/test/java/org/kiwix/kiwixmobile/core/utils/ExternalLinkOpenerTest.kt
@@ -18,4 +18,65 @@
 
 package org.kiwix.kiwixmobile.core.utils
 
-internal class ExternalLinkOpenerTest
+import android.app.Activity
+import android.content.Intent
+import android.widget.Toast
+import io.mockk.Runs
+import io.mockk.every
+import io.mockk.just
+import io.mockk.mockk
+import io.mockk.mockkStatic
+import io.mockk.spyk
+import io.mockk.verify
+import org.junit.jupiter.api.Test
+import org.kiwix.kiwixmobile.core.R
+import org.kiwix.kiwixmobile.core.extensions.toast
+
+internal class ExternalLinkOpenerTest {
+  private val sharedPreferenceUtil: SharedPreferenceUtil = mockk()
+  private val alertDialogShower: AlertDialogShower = mockk()
+  private val intent: Intent = mockk()
+  private val activity: Activity = mockk()
+
+  @Test
+  internal fun `requestOpenLink is called if external link popup preference is true`() {
+    every { intent.resolveActivity(activity.packageManager) } returns mockk()
+    every { sharedPreferenceUtil.prefExternalLinkPopup } returns true
+    val externalLinkOpener =
+      spyk(
+        ExternalLinkOpener(activity, sharedPreferenceUtil, alertDialogShower),
+        recordPrivateCalls = true
+      )
+    externalLinkOpener.openExternalUrl(intent)
+    verify { externalLinkOpener["requestOpenLink"](intent) }
+  }
+
+  @Test
+  internal fun `openLink is called if external link popup preference is false`() {
+    every { intent.resolveActivity(activity.packageManager) } returns mockk()
+    every { sharedPreferenceUtil.prefExternalLinkPopup } returns false
+    val externalLinkOpener =
+      spyk(
+        ExternalLinkOpener(activity, sharedPreferenceUtil, alertDialogShower),
+        recordPrivateCalls = true
+      )
+    externalLinkOpener.openExternalUrl(intent)
+    verify { externalLinkOpener["openLink"](intent) }
+  }
+
+  @Test
+  internal fun `toast if packageManager is null`() {
+    every { intent.resolveActivity(activity.packageManager) } returns null
+    val externalLinkOpener =
+      spyk(
+        ExternalLinkOpener(activity, sharedPreferenceUtil, alertDialogShower),
+        recordPrivateCalls = true
+      )
+    mockkStatic(Toast::class)
+    every {
+      Toast.makeText(activity, R.string.no_reader_application_installed, Toast.LENGTH_LONG).show()
+    } just Runs
+    externalLinkOpener.openExternalUrl(intent)
+    verify { activity.toast(R.string.no_reader_application_installed) }
+  }
+}
