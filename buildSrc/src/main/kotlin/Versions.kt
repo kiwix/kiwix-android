03f97027a248d094d1d9f90946d787674eced5c3
Sean Mac Gillicuddy <seantheappdev@gmail.com>
2020-08-05 11:05:06 +0100

    #2222 Impossible to read warc2zim generated ZIM files - support service workers - load war2c as asset

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -16,17 +16,19 @@ object Versions {
 
   const val com_squareup_retrofit2: String = "2.5.0" // available: "2.9.0"
 
+  const val com_squareup_okhttp3: String = "3.6.0" // available: "4.8.0"
+
   const val org_jetbrains_kotlin: String = "1.3.72"
 
-  const val com_google_dagger: String = "2.26" // available: "2.28.1"
+  const val com_google_dagger: String = "2.26" // available: "2.28.3"
 
   const val com_yahoo_squidb: String = "2.0.0" // available: "3.2.3"
 
-  const val com_jakewharton: String = "10.2.1"
+  const val com_jakewharton: String = "10.2.1" // available: "10.2.2"
 
   const val androidx_test: String = "1.2.0"
 
-  const val io_objectbox: String = "2.6.0"
+  const val io_objectbox: String = "2.6.0" // available: "2.7.0"
 
   const val org_jacoco: String = "0.7.9"
 
@@ -44,8 +46,6 @@ object Versions {
 
   const val javax_annotation_api: String = "1.3.2"
 
-  const val logging_interceptor: String = "3.6.0" // available: "4.7.2"
-
   const val ink_page_indicator: String = "1.3.0"
 
   const val leakcanary_android: String = "2.4"
@@ -58,8 +58,6 @@ object Versions {
 
   const val junit_jupiter: String = "5.6.2"
 
-  const val mockwebserver: String = "3.6.0" // available: "4.7.2"
-
   const val xfetch2okhttp: String = "3.1.4"
 
   const val assertj_core: String = "3.16.1"
@@ -68,7 +66,7 @@ object Versions {
 
   const val fragment_ktx: String = "1.2.5"
 
-  const val lint_gradle: String = "27.0.0"
+  const val lint_gradle: String = "27.0.1"
 
   const val testing_ktx: String = "1.1.2"
 
@@ -84,11 +82,11 @@ object Versions {
 
   const val rxandroid: String = "2.1.1"
 
-  const val core_ktx: String = "1.3.0"
+  const val core_ktx: String = "1.3.0" // available: "1.3.1"
 
-  const val kiwixlib: String = "9.2.3"
+  const val kiwixlib: String = "9.2.3" // available: "9.3.1"
 
-  const val material: String = "1.1.0"
+  const val material: String = "1.1.0" // available: "1.2.0"
 
   const val multidex: String = "2.0.1"
 
@@ -100,16 +98,16 @@ object Versions {
 
   const val ktlint: String = "0.36.0" // available: "0.37.2"
 
-  const val okhttp: String = "3.6.0" // available: "4.7.2"
-
   const val rxjava: String = "2.2.19"
 
-  const val aapt2: String = "4.0.0-6051327"
+  const val webkit: String = "1.2.0"
+
+  const val aapt2: String = "4.0.1-6197926"
 
   const val junit: String = "1.1.1"
 
   /**
-   * Current version: "6.1.1"
+   * Current version: "6.2"
    * See issue 19: How to update Gradle itself?
    * https://github.com/jmfayard/buildSrcVersions/issues/19
    */
