97c90901e685c855974a7e602120186f73e5ef97
bharath2547 <bharath.pulluru@gmail.com>
2020-08-21 20:13:09 +0530

    converted DocumentParser.java class to kotlin class

--- core/src/main/java/org/kiwix/kiwixmobile/core/main/CoreReaderFragment.java
+++ core/src/main/java/org/kiwix/kiwixmobile/core/main/CoreReaderFragment.java
@@ -467,8 +467,9 @@ public abstract class CoreReaderFragment extends BaseFragment
 
   private void setupDocumentParser() {
     documentParser = new DocumentParser(new DocumentParser.SectionsListener() {
+
       @Override
-      public void sectionsLoaded(String title, List<TableDrawerAdapter.DocumentSection> sections) {
+      public void sectionsLoaded(String title, List<? extends TableDrawerAdapter.DocumentSection> sections) {
         if (isAdded()) {
           for (TableDrawerAdapter.DocumentSection section : sections) {
             if (section.title.contains("REPLACE_")) {
---
+++ core/src/main/java/org/kiwix/kiwixmobile/core/main/DocumentParser.kt
@@ -0,0 +1,84 @@
+/*
+ * Kiwix Android
+ * Copyright (c) 2020 Kiwix <android.kiwix.org>
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+package org.kiwix.kiwixmobile.core.main
+
+import android.os.Handler
+import android.os.Looper
+import android.webkit.JavascriptInterface
+import android.webkit.WebView
+import org.kiwix.kiwixmobile.core.main.TableDrawerAdapter.DocumentSection
+
+public class DocumentParser(private var listener: DocumentParser.SectionsListener) {
+
+  private var tittle: String = ""
+  private var sections = ArrayList<TableDrawerAdapter.DocumentSection>();
+
+  public fun initInterface(webView: WebView) {
+    webView.addJavascriptInterface(ParserCallback(), "Documentparser")
+  }
+
+  public interface SectionsListener {
+    fun sectionsLoaded(tittle: String = "", sections: List<DocumentSection>)
+
+    fun clearSections()
+  }
+
+  inner class ParserCallback() {
+
+    @JavascriptInterface
+    public fun parse(sectionTittle: String, element: String, id: String) {
+
+      if (element == "H1") {
+        tittle = sectionTittle.trim()
+        return
+      }
+      val section = DocumentSection()
+      section.title = section.title.trim();
+      section.id = id;
+      var level: Int
+      try {
+        val character: String = element.substring(element.length - 1)
+        level = Integer.parseInt(character)
+        Integer.parseInt(id)
+      } catch (e: NumberFormatException) {
+        level = 0
+      }
+      section.level = level;
+      sections.add(section)
+    }
+
+    @JavascriptInterface
+    public fun start() {
+      tittle = ""
+      sections = ArrayList()
+      Handler(Looper.getMainLooper()).post(Runnable(listener::clearSections))
+    }
+
+    @JavascriptInterface
+    public fun stop() {
+      val listToBeSentToMainThread: List<DocumentSection> = ArrayList(sections);
+      Handler(Looper.getMainLooper()).post {
+        listener.sectionsLoaded(
+          tittle,
+          listToBeSentToMainThread
+        )
+      }
+    }
+  }
+}
