1c27071e185d77d753b54a529fbef5a310e7a343
HissPirat <franslukas@gmail.com>
2020-09-15 16:08:41 +0200

    #2280 seeing if ignoring intro fragment test results in inst. test fix.

--- app/src/androidTest/java/org/kiwix/kiwixmobile/intro/IntroFragmentTest.kt
+++ app/src/androidTest/java/org/kiwix/kiwixmobile/intro/IntroFragmentTest.kt
@@ -18,6 +18,7 @@
 package org.kiwix.kiwixmobile.intro
 
 import androidx.test.internal.runner.junit4.statement.UiThreadStatement.runOnUiThread
+import org.junit.Ignore
 import org.junit.Test
 import org.kiwix.kiwixmobile.BaseActivityTest
 import org.kiwix.kiwixmobile.R
@@ -28,10 +29,9 @@ class IntroFragmentTest : BaseActivityTest<KiwixMainActivity>() {
   override var activityRule = activityTestRule<KiwixMainActivity>()
 
   @Test
+  @Ignore("Testing if this breaks most other tests")
   fun viewIsSwipeableAndNavigatesToMain() {
     runOnUiThread { activityRule.activity.navigate(R.id.introFragment) }
-    intro {
-      swipeLeft()
-    } clickGetStarted { }
+    intro(IntroRobot::swipeLeft) clickGetStarted { }
   }
 }
