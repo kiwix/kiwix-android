4c7e2340e6f1a08a95dc2fbef5b191506a50405f
gouri-panda <gouripanda4@gmail.com>
2020-09-16 16:04:35 +0530

    #2361 formated code

--- core/src/main/java/org/kiwix/kiwixmobile/core/main/KiwixWebView.kt
+++ core/src/main/java/org/kiwix/kiwixmobile/core/main/KiwixWebView.kt
@@ -167,10 +167,7 @@ open class KiwixWebView @SuppressLint("SetJavaScriptEnabled") constructor(
         val fileToSave = sequence {
           yield(File(root, fileName))
           yieldAll(generateSequence(1) { it + 1 }.map {
-            File(
-              root,
-              fileName.replace(".", "_$it.")
-            )
+            File(root, fileName.replace(".", "_$it."))
           })
         }.first { !it.exists() }
         val source = Uri.parse(src)
