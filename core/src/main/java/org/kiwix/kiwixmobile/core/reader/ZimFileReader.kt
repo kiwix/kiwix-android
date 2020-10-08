4541d939f4b407dfba2e06761cc8aca557f15ca7
Sean Mac Gillicuddy <seantheappdev@gmail.com>
2020-10-08 10:08:50 +0100

    #2303 re-enable support for service workers

--- core/src/main/java/org/kiwix/kiwixmobile/core/reader/ZimFileReader.kt
+++ core/src/main/java/org/kiwix/kiwixmobile/core/reader/ZimFileReader.kt
@@ -236,7 +236,7 @@ class ZimFileReader constructor(
     @JvmField
     val UI_URI: Uri? = Uri.parse("content://org.kiwix.ui/")
 
-    const val CONTENT_PREFIX = "zim://content/"
+    const val CONTENT_PREFIX = "https://kiwix.app/"
 
     private val INVERT_IMAGES_VIDEO =
       """
