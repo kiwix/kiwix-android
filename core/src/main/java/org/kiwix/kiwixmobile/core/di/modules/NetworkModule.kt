cdcc45cf92fafe3da9d1a0be5e5dc654f7cfdafb
Sean Mac Gillicuddy <seantheappdev@gmail.com>
2020-11-24 12:10:17 +0000

    #2385 Online Library is just loading and loading - make serialiser strict

--- core/src/main/java/org/kiwix/kiwixmobile/core/di/modules/NetworkModule.kt
+++ core/src/main/java/org/kiwix/kiwixmobile/core/di/modules/NetworkModule.kt
@@ -32,15 +32,19 @@ import javax.inject.Singleton
 
 private const val CONNECTION_TIMEOUT = 10L
 private const val READ_TIMEOUT = 60L
+private const val CALL_TIMEOUT = 60L
 private const val USER_AGENT = "kiwix-android-version:${BuildConfig.VERSION_CODE}"
 private const val KIWIX_DOWNLOAD_URL = "http://mirror.download.kiwix.org/"
 
 @Module
 class NetworkModule {
   @Provides @Singleton fun provideOkHttpClient(): OkHttpClient {
-    return OkHttpClient().newBuilder().followRedirects(true).followSslRedirects(true)
+    return OkHttpClient().newBuilder()
+      .followRedirects(true)
+      .followSslRedirects(true)
       .connectTimeout(CONNECTION_TIMEOUT, SECONDS)
       .readTimeout(READ_TIMEOUT, SECONDS)
+      .callTimeout(CALL_TIMEOUT, SECONDS)
       .addNetworkInterceptor(HttpLoggingInterceptor().apply {
         level = if (BuildConfig.DEBUG) BASIC else NONE
       })
