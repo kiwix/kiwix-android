53ae91dbea50e495cb6596efc96e64e66b035a6d
MohitMaliFtechiz <futuretechnoworld.mohit@gmail.com>
2022-07-06 13:03:23 +0530

    CI to check kotlin version

---
+++ contrib/kotlin-validator.sh
@@ -0,0 +1,49 @@
+#
+# Kiwix Android
+# Copyright (c) 2022 Kiwix <android.kiwix.org>
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+#
+
+ #!/usr/bin/env bash
+
+ #
+ # Kiwix Android
+ # Copyright (c) 2022 Kiwix <android.kiwix.org>
+ # This program is free software: you can redistribute it and/or modify
+ # it under the terms of the GNU General Public License as published by
+ # the Free Software Foundation, either version 3 of the License, or
+ # (at your option) any later version.
+ #
+ # This program is distributed in the hope that it will be useful,
+ # but WITHOUT ANY WARRANTY; without even the implied warranty of
+ # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ # GNU General Public License for more details.
+ #
+ # You should have received a copy of the GNU General Public License
+ # along with this program. If not, see <http://www.gnu.org/licenses/>.
+ #
+ #
+
+ if ./gradlew buildKotlinToolingMetadata; then
+   version=$(cat app/build/kotlinToolingMetadata/kotlin-tooling-metadata.json | tr { '\n' | tr , '\n' | tr } '\n' | grep "buildPluginVersion" | awk  -F'"' '{print $4}')
+   if [ $version == "1.7.0" ];
+   then
+    echo "Build System is using kotlin version 1.7.0"
+   else
+     exit 1
+   fi
+ else
+   exit 1
+ fi
