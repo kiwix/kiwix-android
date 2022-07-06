ed007fd8513e043c2b2031c1981961ece03b2c12
MohitMaliFtechiz <futuretechnoworld.mohit@gmail.com>
2022-07-06 14:30:24 +0530

    fixing code factor

--- contrib/kotlin-validator.sh
+++ contrib/kotlin-validator.sh
@@ -1,24 +1,4 @@
-#
-# Kiwix Android
-# Copyright (c) 2022 Kiwix <android.kiwix.org>
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program. If not, see <http://www.gnu.org/licenses/>.
-#
-#
-
- #!/usr/bin/env bash
-
- #
+#!/usr/bin/env bash
  # Kiwix Android
  # Copyright (c) 2022 Kiwix <android.kiwix.org>
  # This program is free software: you can redistribute it and/or modify
@@ -42,7 +22,7 @@
    then
     echo "Build System is using kotlin Version 1.7.0"
    else
-    echo "Build Failure : Due to kotlin Version 1.7.0 is missing"
+     echo "Build Failure : Due to kotlin Version 1.7.0 is missing"
      exit 1
    fi
  else
