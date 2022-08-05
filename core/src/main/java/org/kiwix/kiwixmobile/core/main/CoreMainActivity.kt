31ba1bc2df6567effec74de4e21878dcedc44ff3
MohitMali <futuretechnoworld.mohit@gmail.com>
2022-08-05 16:18:32 +0530

    Search screen not popup after pressing back from search result

--- core/src/main/java/org/kiwix/kiwixmobile/core/main/CoreMainActivity.kt
+++ core/src/main/java/org/kiwix/kiwixmobile/core/main/CoreMainActivity.kt
@@ -229,7 +229,10 @@ abstract class CoreMainActivity : BaseActivity(), WebViewProvider {
     }
     activeFragments().filterIsInstance<FragmentActivityExtensions>().forEach {
       if (it.onBackPressed(this) == FragmentActivityExtensions.Super.ShouldCall) {
-        if (navController.currentDestination?.label?.equals("Reader") == true) {
+        if (navController.currentDestination?.label?.equals("Reader") == true &&
+          navController.previousBackStackEntry?.destination
+            ?.label?.equals("SearchFragment") == false
+        ) {
           finish()
         } else {
           super.onBackPressed()
