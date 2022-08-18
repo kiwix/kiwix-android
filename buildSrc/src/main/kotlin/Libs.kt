c9ccfe7cca8c298f25095d8f663e1dfad255b531
MohitMali <futuretechnoworld.mohit@gmail.com>
2022-08-18 19:23:12 +0530

    Remove Play Core Services

--- buildSrc/src/main/kotlin/Libs.kt
+++ buildSrc/src/main/kotlin/Libs.kt
@@ -420,10 +420,4 @@ object Libs {
    * https://developer.android.com/testing
    */
   const val junit: String = "androidx.test.ext:junit:" + Versions.junit
-
-  /**
-   * https://developer.android.com/reference/com/google/android/play/core/release-notes
-   */
-  const val google_android_play_core: String =
-    "com.google.android.play:core:" + Versions.google_android_play_core
 }
