1105752564aefa45bf5f609e2a493304da6fbd3f
MohitMali <futuretechnoworld.mohit@gmail.com>
2023-01-11 12:39:52 +0530

    Added retry in bash script

--- contrib/instrumentation.sh
+++ contrib/instrumentation.sh
@@ -2,9 +2,21 @@
 
 adb logcat -c
 adb logcat *:E -v color &
-if ./gradlew jacocoInstrumentationTestReport; then
-  echo "jacocoInstrumentationTestReport succeeded" >&2
-else
-  adb exec-out screencap -p >screencap.png
-  exit 1
-fi
+retry=0
+while [ $retry -le 3 ]
+do
+  if ./gradlew jacocoInstrumentationTestReport; then
+    echo "jacocoInstrumentationTestReport succeeded" >&2
+    break
+  else
+    adb kill-server
+    adb start-server
+    adb logcat -c
+    ./gradlew clean
+    retry=$(( $retry + 1 ))
+    if [ $retry == 3 ]; then
+      adb exec-out screencap -p >screencap.png
+      exit 1
+    fi
+  fi
+done
