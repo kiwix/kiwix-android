7e9aee96fa5d1117e5cec9a356758d77fa568da3
MohitMali <futuretechnoworld.mohit@gmail.com>
2023-10-02 16:12:08 +0530

    We have fixed the issue of downloading not working in the custom app for the DWDS application. We had added the BasicAuthInterceptor to the NetworkModule, but it is responsible for resolving the URLs. However, for downloading the ZIM files, we need this BasicAuthInterceptor in the OkHttp client of the DownloaderModule which is responsible for downloading zim files.

--- core/src/main/java/org/kiwix/kiwixmobile/core/di/modules/NetworkModule.kt
+++ core/src/main/java/org/kiwix/kiwixmobile/core/di/modules/NetworkModule.kt
@@ -24,7 +24,6 @@ import okhttp3.logging.HttpLoggingInterceptor
 import okhttp3.logging.HttpLoggingInterceptor.Level.BASIC
 import okhttp3.logging.HttpLoggingInterceptor.Level.NONE
 import org.kiwix.kiwixmobile.core.BuildConfig
-import org.kiwix.kiwixmobile.core.data.remote.BasicAuthInterceptor
 import org.kiwix.kiwixmobile.core.data.remote.KiwixService
 import org.kiwix.kiwixmobile.core.data.remote.KiwixService.ServiceCreator
 import org.kiwix.kiwixmobile.core.data.remote.UserAgentInterceptor
@@ -52,7 +51,6 @@ class NetworkModule {
         }
       )
       .addNetworkInterceptor(UserAgentInterceptor(USER_AGENT))
-      .addInterceptor(BasicAuthInterceptor())
       .build()
   }
 
