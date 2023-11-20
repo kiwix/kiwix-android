a56b5615700fd5d1963ae1cc129ea165bafcc2ad
MohitMali <futuretechnoworld.mohit@gmail.com>
2023-11-20 17:15:25 +0530

    Fixed detekt issues.

--- core/src/main/java/org/kiwix/kiwixmobile/core/main/OnSwipeTouchListener.kt
+++ core/src/main/java/org/kiwix/kiwixmobile/core/main/OnSwipeTouchListener.kt
@@ -52,8 +52,7 @@ open class OnSwipeTouchListener constructor(context: Context) : OnTouchListener 
     }
 
     // See:- https://stackoverflow.com/questions/73463685/gesturedetector-ongesturelistener-overridden-methods-are-not-working-in-android
-    @Suppress("NOTHING_TO_OVERRIDE", "ACCIDENTAL_OVERRIDE")
-    @SuppressLint("NestedBlockDepth, ReturnCount")
+    @Suppress("NOTHING_TO_OVERRIDE", "ACCIDENTAL_OVERRIDE", "NestedBlockDepth", "ReturnCount")
     override fun onFling(
       e1: MotionEvent,
       e2: MotionEvent,
