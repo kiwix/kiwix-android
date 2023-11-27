3cab50e195c95799005e947b7b0bc06d3ba12ad1
MohitMaliFtechiz <futuretechnoworl.mohit@gmail.com>
2023-11-27 14:30:20 +0530

    Fixed, FileObserver constructor issue comes after upgrading the gradle to 8.1.3

--- app/src/main/java/org/kiwix/kiwixmobile/zimManager/Fat32Checker.kt
+++ app/src/main/java/org/kiwix/kiwixmobile/zimManager/Fat32Checker.kt
@@ -18,6 +18,7 @@
 package org.kiwix.kiwixmobile.zimManager
 
 import android.annotation.SuppressLint
+import android.os.Build
 import android.os.FileObserver
 import io.reactivex.Flowable
 import io.reactivex.functions.BiFunction
@@ -62,12 +63,21 @@ class Fat32Checker constructor(
       )
   }
 
+  @Suppress("DEPRECATION")
   private fun fileObserver(it: String): FileObserver =
-    object : FileObserver(File(it), MOVED_FROM or DELETE) {
-      override fun onEvent(event: Int, path: String?) {
-        requestCheckSystemFileType.onNext(Unit)
-      }
-    }.apply { startWatching() }
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+      object : FileObserver(File(it), MOVED_FROM or DELETE) {
+        override fun onEvent(event: Int, path: String?) {
+          requestCheckSystemFileType.onNext(Unit)
+        }
+      }.apply { startWatching() }
+    } else {
+      object : FileObserver(it, MOVED_FROM or DELETE) {
+        override fun onEvent(event: Int, path: String?) {
+          requestCheckSystemFileType.onNext(Unit)
+        }
+      }.apply { startWatching() }
+    }
 
   private fun toFileSystemState(it: String) =
     when {
