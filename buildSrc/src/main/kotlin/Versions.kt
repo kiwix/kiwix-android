23ede348cc4107147f43f0779fdd62ef5b8b367b
MohitMaliFtechiz <futuretechnoworld.mohit@gmail.com>
2024-02-06 12:51:24 +0530

    Upgraded java-libkiwix to 2.0.0
    * A new version of java-libkiwix has been released so we are using the new version of java-libkiwix in our project.

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -86,7 +86,7 @@ object Versions {
 
   const val core_ktx: String = "1.3.2"
 
-  const val libkiwix: String = "1.0.0"
+  const val libkiwix: String = "2.0.0"
 
   const val material: String = "1.2.1"
 
