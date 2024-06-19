eb95bddf6ca6012ae9d817f8bab9f67af6186644
MohitMaliFtechiz <futuretechnoworld.mohit@gmail.com>
2024-06-19 19:05:49 +0530

    Added unit test cases for testing the previous job is cancelled properly.

--- core/src/test/java/org/kiwix/kiwixmobile/core/search/viewmodel/SearchStateTest.kt
+++ core/src/test/java/org/kiwix/kiwixmobile/core/search/viewmodel/SearchStateTest.kt
@@ -20,9 +20,13 @@ package org.kiwix.kiwixmobile.core.search.viewmodel
 
 import io.mockk.every
 import io.mockk.mockk
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.cancelAndJoin
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.test.runTest
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import org.kiwix.kiwixmobile.core.search.adapter.SearchListItem
 import org.kiwix.kiwixmobile.core.search.adapter.SearchListItem.RecentSearchListItem
 import org.kiwix.kiwixmobile.core.search.viewmodel.SearchOrigin.FromWebView
 
@@ -96,5 +100,43 @@ internal class SearchStateTest {
         FromWebView
       ).isLoading
     ).isFalse
+  }
+
+  @Test
+  fun `test search cancellation`() = runTest {
+    val searchTerm = "cancelTest"
+    val suggestionSearchWrapper: SuggestionSearchWrapper = mockk()
+    val searchIteratorWrapper: SuggestionIteratorWrapper = mockk()
+    val entryWrapper: SuggestionItemWrapper = mockk()
+
+    every { suggestionSearchWrapper.estimatedMatches } returns 100
+    every { searchIteratorWrapper.hasNext() } returnsMany listOf(true, false)
+    every { searchIteratorWrapper.next() } returns entryWrapper
+    every { entryWrapper.title } returns "Result"
+    every { entryWrapper.path } returns "path"
+    every { suggestionSearchWrapper.getResults(any(), any()) } returns searchIteratorWrapper
+
+    val searchResultsWithTerm = SearchResultsWithTerm(searchTerm, suggestionSearchWrapper, mockk())
+    val searchState = SearchState(searchTerm, searchResultsWithTerm, emptyList(), FromWebView)
+    var list: List<SearchListItem.RecentSearchListItem>? = emptyList()
+    var list1: List<SearchListItem.RecentSearchListItem>? = emptyList()
+    val job = launch(Dispatchers.IO) {
+      list = searchState.getVisibleResults(0)
+    }
+
+    val job1 = launch(Dispatchers.IO) {
+      list1 = searchState.getVisibleResults(0)
+    }
+    job.cancelAndJoin()
+
+    // test the coroutine job is cancelled properly
+    assertThat(job.isCancelled).isTrue
+    assertThat(list?.size).isEqualTo(0)
+
+    // test the second job is successfully return the data
+    assertThat(job1.isCompleted).isTrue
+    assertThat(list1?.size).isEqualTo(1)
+    assertThat(list1?.get(0)?.url).isEqualTo("path")
+    assertThat(list1?.get(0)?.value).isEqualTo("Result")
   }
 }
