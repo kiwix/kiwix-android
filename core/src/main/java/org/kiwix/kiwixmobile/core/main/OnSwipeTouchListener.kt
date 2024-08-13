88a83be01d9679dd1781398b7796d98ddf73903e
MohitMaliFtechiz <futuretechnoworld.mohit@gmail.com>
2024-08-13 14:47:45 +0530

    Updated to Android 14 & refactored some methods inorder to compile with the SDK 14

--- core/src/main/java/org/kiwix/kiwixmobile/core/main/OnSwipeTouchListener.kt
+++ core/src/main/java/org/kiwix/kiwixmobile/core/main/OnSwipeTouchListener.kt
@@ -51,6 +51,9 @@ open class OnSwipeTouchListener constructor(context: Context) : OnTouchListener 
       return super.onSingleTapUp(event)
     }
 
+    // See:- https://stackoverflow.com/questions/73463685/gesturedetector-ongesturelistener-overridden-methods-are-not-working-in-android
+    @Suppress("NOTHING_TO_OVERRIDE", "ACCIDENTAL_OVERRIDE")
+    @SuppressLint("NestedBlockDepth, ReturnCount")
     override fun onFling(
       e1: MotionEvent,
       e2: MotionEvent,
