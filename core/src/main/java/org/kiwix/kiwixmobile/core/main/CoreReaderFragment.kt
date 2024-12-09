ee90904b06b9baa058826eae71941940235e0e64
MohitMaliFtechiz <futuretechnoworld.mohit@gmail.com>
2024-12-09 14:25:59 +0530

    Added debug logs which will help us in debugging if any error happens.

--- core/src/main/java/org/kiwix/kiwixmobile/core/main/CoreReaderFragment.kt
+++ core/src/main/java/org/kiwix/kiwixmobile/core/main/CoreReaderFragment.kt
@@ -2686,6 +2686,10 @@ abstract class CoreReaderFragment :
           findInPageTitle = null
         }
       }, {
+        Log.e(
+          TAG_KIWIX,
+          "Could not restore tabs. Original exception = ${it.printStackTrace()}"
+        )
         restoreViewStateOnInvalidWebViewHistory()
       })
   }
