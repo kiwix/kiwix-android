82b174e3eecfff04e00d1c4edd96177b56524cec
MohitMaliFtechiz <futuretechnoworld.mohit@gmail.com>
2025-01-20 13:08:47 +0530

    Creating the reader in lifecycleScope

--- core/src/main/java/org/kiwix/kiwixmobile/core/page/notes/viewmodel/effects/ShowOpenNoteDialog.kt
+++ core/src/main/java/org/kiwix/kiwixmobile/core/page/notes/viewmodel/effects/ShowOpenNoteDialog.kt
@@ -19,7 +19,9 @@
 package org.kiwix.kiwixmobile.core.page.notes.viewmodel.effects
 
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
 import io.reactivex.processors.PublishProcessor
+import kotlinx.coroutines.launch
 import org.json.JSONArray
 import org.kiwix.kiwixmobile.core.base.SideEffect
 import org.kiwix.kiwixmobile.core.extensions.ActivityExtensions.cachedComponent
@@ -51,36 +53,38 @@ data class ShowOpenNoteDialog(
       ShowNoteDialog,
       { effects.offer(OpenPage(page, zimReaderContainer)) },
       {
-        val item = page as NoteListItem
-        // Check if toDatabase is not null, and then set it in zimReaderContainer.
-        // For custom apps, we are currently using fileDescriptor, and they only have a single file in them,
-        // which is already set in zimReaderContainer, so there's no need to set it again.
-        item.zimReaderSource?.toDatabase().let {
-          val currentZimReaderSource = zimReaderContainer.zimReaderSource
-          if (!activity.isCustomApp()) {
-            zimReaderContainer.setZimReaderSource(item.zimReaderSource)
+        activity.lifecycleScope.launch {
+          val item = page as NoteListItem
+          // Check if toDatabase is not null, and then set it in zimReaderContainer.
+          // For custom apps, we are currently using fileDescriptor, and they only have a single file in them,
+          // which is already set in zimReaderContainer, so there's no need to set it again.
+          item.zimReaderSource?.toDatabase().let {
+            val currentZimReaderSource = zimReaderContainer.zimReaderSource
+            if (!activity.isCustomApp()) {
+              zimReaderContainer.setZimReaderSource(item.zimReaderSource)
+            }
+            if (zimReaderContainer.zimReaderSource != currentZimReaderSource) {
+              // if current zim file is not the same set the main page of that zim file
+              // so that when we go back it properly loads the article, and do nothing if the
+              // zim file is same because there might be multiple tabs opened.
+              val settings = activity.getSharedPreferences(
+                SharedPreferenceUtil.PREF_KIWIX_MOBILE,
+                0
+              )
+              val editor = settings.edit()
+              val urls = JSONArray()
+              val positions = JSONArray()
+              urls.put(CONTENT_PREFIX + zimReaderContainer.mainPage)
+              positions.put(0)
+              editor.putString(TAG_CURRENT_FILE, zimReaderContainer.zimReaderSource?.toDatabase())
+              editor.putString(TAG_CURRENT_ARTICLES, "$urls")
+              editor.putString(TAG_CURRENT_POSITIONS, "$positions")
+              editor.putInt(TAG_CURRENT_TAB, 0)
+              editor.apply()
+            }
           }
-          if (zimReaderContainer.zimReaderSource != currentZimReaderSource) {
-            // if current zim file is not the same set the main page of that zim file
-            // so that when we go back it properly loads the article, and do nothing if the
-            // zim file is same because there might be multiple tabs opened.
-            val settings = activity.getSharedPreferences(
-              SharedPreferenceUtil.PREF_KIWIX_MOBILE,
-              0
-            )
-            val editor = settings.edit()
-            val urls = JSONArray()
-            val positions = JSONArray()
-            urls.put(CONTENT_PREFIX + zimReaderContainer.mainPage)
-            positions.put(0)
-            editor.putString(TAG_CURRENT_FILE, zimReaderContainer.zimReaderSource?.toDatabase())
-            editor.putString(TAG_CURRENT_ARTICLES, "$urls")
-            editor.putString(TAG_CURRENT_POSITIONS, "$positions")
-            editor.putInt(TAG_CURRENT_TAB, 0)
-            editor.apply()
-          }
+          effects.offer(OpenNote(item.noteFilePath, item.zimUrl, item.title))
         }
-        effects.offer(OpenNote(item.noteFilePath, item.zimUrl, item.title))
       }
     )
   }
