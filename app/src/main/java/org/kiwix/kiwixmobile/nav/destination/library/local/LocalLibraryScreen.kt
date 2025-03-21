7d7f726d515ab5c01d1d32c0cde7ce853ecf8e6d
MohitMaliFtechiz <futuretechnoworld.mohit@gmail.com>
2025-03-21 17:01:37 +0530

    Fixed: SwipeRefreshLayout was not working when the "No Files Here" text was displayed. This was because, by default, this component does not have verticalScroll, which is required to enable the swipe-to-refresh functionality.

--- app/src/main/java/org/kiwix/kiwixmobile/nav/destination/library/local/LocalLibraryScreen.kt
+++ app/src/main/java/org/kiwix/kiwixmobile/nav/destination/library/local/LocalLibraryScreen.kt
@@ -28,6 +28,8 @@ import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
@@ -209,7 +211,10 @@ fun NoFilesView(
   onDownloadButtonClick: () -> Unit
 ) {
   Column(
-    modifier = Modifier.fillMaxSize().padding(horizontal = FOUR_DP),
+    modifier = Modifier
+      .fillMaxSize()
+      .padding(horizontal = FOUR_DP)
+      .verticalScroll(rememberScrollState()),
     horizontalAlignment = Alignment.CenterHorizontally,
     verticalArrangement = Arrangement.Center
   ) {
