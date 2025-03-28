cfd9431f58022e9a10fe107ac83490b39eff2cdd
MohitMaliFtechiz <futuretechnoworld.mohit@gmail.com>
2025-03-28 10:48:41 +0530

    Upgraded gradle to `8.10.0-alpha08` to support Android 16 properly.

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -48,7 +48,7 @@ object Versions {
 
   const val android_arch_lifecycle_extensions: String = "1.1.1"
 
-  const val com_android_tools_build_gradle: String = "8.10.0-alpha06"
+  const val com_android_tools_build_gradle: String = "8.10.0-alpha08"
 
   const val de_fayard_buildsrcversions_gradle_plugin: String = "0.7.0"
 
