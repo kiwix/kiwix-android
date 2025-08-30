8204b8533bd133991003be8e959ad9c637fe5ce0
MohitMaliFtechiz <futuretechnoworld.mohit@gmail.com>
2025-08-30 12:23:55 +0530

    Updated the `README` file to reflect the compatible version of Android Studio.
    * There is now an official IDE that supports Android 16, so we updated our README file accordingly.
    * Updated the Gradle version from `8.11.0-alpha03` to `8.11.1`, which is the stable release for Android 16.

--- core/src/main/java/org/kiwix/kiwixmobile/core/main/CoreMainActivity.kt
+++ core/src/main/java/org/kiwix/kiwixmobile/core/main/CoreMainActivity.kt
@@ -166,7 +166,7 @@ abstract class CoreMainActivity : BaseActivity(), WebViewProvider {
   @Inject
   lateinit var downloadMonitor: DownloadMonitor
 
-  @Suppress("InjectDispatcher")
+  @Suppress("InjectDispatcher", "DefaultUncaughtExceptionDelegation")
   override fun onCreate(savedInstanceState: Bundle?) {
     setTheme(R.style.KiwixTheme)
     super.onCreate(savedInstanceState)
