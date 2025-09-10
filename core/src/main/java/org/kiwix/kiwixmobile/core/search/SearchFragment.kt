cc693a002d8555cd731a5aef45640064eb683180
MohitMaliFtechiz <futuretechnoworld.mohit@gmail.com>
2025-09-11 01:15:49 +0530

    Improved search performance on low-memory devices.
    * Both the initial search (while typing) and loading of additional items have been moved to the IO thread, keeping the UI thread free and ensuring a smoother user experience.
    * Optimized result fetching from libkiwix: previously, 100 results were retrieved for every typed character. This was inefficient, as users often continue typing and most of those results went unused.
    * Even though ongoing requests were canceled when users typed further, fetching 100 results at a time was still wasteful. Once the native C++ code started processing, it consumed memory and slowed performance, regardless of cancellation.
    * The default has now been adjusted to fetch 20 results per search instead of 100. This reduces memory usage, improves response time, and remains practical since users cannot view 100 results at once. If additional results are needed, they can simply scroll, and more items will be loaded through the loadMore functionality.

--- core/src/main/java/org/kiwix/kiwixmobile/core/search/SearchFragment.kt
+++ core/src/main/java/org/kiwix/kiwixmobile/core/search/SearchFragment.kt
@@ -30,16 +30,15 @@ import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
 import kotlinx.coroutines.CancellationException
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.cancelAndJoin
 import kotlinx.coroutines.cancelChildren
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 import org.kiwix.kiwixmobile.core.R
 import org.kiwix.kiwixmobile.core.base.BaseActivity
 import org.kiwix.kiwixmobile.core.base.BaseFragment
 import org.kiwix.kiwixmobile.core.base.FragmentActivityExtensions
+import org.kiwix.kiwixmobile.core.downloader.downloadManager.ZERO
 import org.kiwix.kiwixmobile.core.extensions.ActivityExtensions.cachedComponent
 import org.kiwix.kiwixmobile.core.extensions.closeKeyboard
 import org.kiwix.kiwixmobile.core.extensions.coreMainActivity
@@ -268,7 +267,6 @@ class SearchFragment : BaseFragment() {
     }
   )
 
-  @Suppress("InjectDispatcher")
   private suspend fun render(state: SearchState) {
     renderingJob?.apply {
       // cancel the children job. Since we are getting the result on IO thread
@@ -285,9 +283,7 @@ class SearchFragment : BaseFragment() {
     // To avoid unnecessary data loading and prevent crashes, we check if the search screen is
     // visible to the user before proceeding. If the screen is not visible,
     // we skip the data loading process.
-    if (!isVisible) {
-      return
-    }
+    if (!isVisible) return
     isDataLoading.value = false
     findInPageMenuItem.value = findInPageMenuItem.value.first to (state.searchOrigin == FromWebView)
     setIsPageSearchEnabled(state.searchTerm)
@@ -295,12 +291,7 @@ class SearchFragment : BaseFragment() {
     renderingJob =
       lifecycleScope.launch {
         try {
-          val searchResult =
-            withContext(Dispatchers.IO) {
-              state.getVisibleResults(0, coroutineContext[Job])
-            }
-
-          searchScreenState.update { copy(isLoading = false) }
+          val searchResult = state.getVisibleResults(ZERO, coroutineContext[Job])
           searchResult?.let {
             searchScreenState.update {
               copy(searchList = it)
