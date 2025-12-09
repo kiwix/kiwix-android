639bec8f116c62e3cab366f7e64afac05fa602b8
jas <jaskq@proton.me>
2025-12-09 19:55:53 +0530

    added shared preferences

--- core/src/main/java/org/kiwix/kiwixmobile/core/CoreApp.kt
+++ core/src/main/java/org/kiwix/kiwixmobile/core/CoreApp.kt
@@ -28,6 +28,7 @@ import org.kiwix.kiwixmobile.core.di.components.CoreComponent
 import org.kiwix.kiwixmobile.core.di.components.DaggerCoreComponent
 import org.kiwix.kiwixmobile.core.main.CoreMainActivity
 import org.kiwix.kiwixmobile.core.utils.files.FileLogger
+import org.kiwix.kiwixmobile.core.utils.workManager.UpdateWorkManager
 import javax.inject.Inject
 
 @Suppress("UnnecessaryAbstractClass")
@@ -64,6 +65,7 @@ abstract class CoreApp : Application() {
 
   override fun onCreate() {
     super.onCreate()
+    UpdateWorkManager.startPeriodicWork(this)
     instance = this
     coreComponent = DaggerCoreComponent.builder()
       .context(this)
