3bdc29bcaf781ac6f23402072d66e99ff57604c7
jas <jaskq@proton.me>
2025-12-09 19:55:53 +0530

    added shared preferences

--- core/src/main/java/org/kiwix/kiwixmobile/core/utils/workManager/UpdateWorkManager.kt
+++ core/src/main/java/org/kiwix/kiwixmobile/core/utils/workManager/UpdateWorkManager.kt
@@ -1,36 +1,78 @@
 package org.kiwix.kiwixmobile.core.utils.workManager
 
 import android.content.Context
-import android.util.Log
 import androidx.work.CoroutineWorker
+import androidx.work.ExistingPeriodicWorkPolicy
 import androidx.work.PeriodicWorkRequest
 import androidx.work.PeriodicWorkRequestBuilder
 import androidx.work.WorkManager
 import androidx.work.WorkerParameters
+import dagger.assisted.Assisted
+import dagger.assisted.AssistedInject
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import okhttp3.logging.HttpLoggingInterceptor.Level.BASIC
+import okhttp3.logging.HttpLoggingInterceptor.Level.NONE
+import org.kiwix.kiwixmobile.core.BuildConfig
+import org.kiwix.kiwixmobile.core.data.remote.KiwixService
+import org.kiwix.kiwixmobile.core.data.remote.UserAgentInterceptor
+import org.kiwix.kiwixmobile.core.di.modules.CALL_TIMEOUT
+import org.kiwix.kiwixmobile.core.di.modules.CONNECTION_TIMEOUT
+import org.kiwix.kiwixmobile.core.di.modules.KIWIX_UPDATE_URL
+import org.kiwix.kiwixmobile.core.di.modules.READ_TIMEOUT
+import org.kiwix.kiwixmobile.core.di.modules.USER_AGENT
 import java.util.concurrent.TimeUnit
+import java.util.concurrent.TimeUnit.SECONDS
 
-class UpdateWorkManager(appContext: Context, workerParams: WorkerParameters) :
-  CoroutineWorker(appContext, workerParams) {
+@Suppress()
+class UpdateWorkManager @AssistedInject constructor(
+  @Assisted appContext: Context,
+  @Assisted workerParams: WorkerParameters,
+  private var kiwixService: KiwixService
+) : CoroutineWorker(appContext, workerParams) {
   override suspend fun doWork(): Result {
-    Log.d("TAG", "doWork: executed")
+    kiwixService =
+      KiwixService.ServiceCreator.newHackListService(
+        okHttpClient = getOkHttpClient(),
+        KIWIX_UPDATE_URL
+      )
     return Result.success()
   }
 
+  @Suppress()
   companion object {
+    fun getOkHttpClient() = OkHttpClient().newBuilder()
+      .followRedirects(true)
+      .followSslRedirects(true)
+      .connectTimeout(CONNECTION_TIMEOUT, SECONDS)
+      .readTimeout(READ_TIMEOUT, SECONDS)
+      .callTimeout(CALL_TIMEOUT, SECONDS)
+      .addNetworkInterceptor(
+        HttpLoggingInterceptor().apply {
+          level = if (BuildConfig.DEBUG) BASIC else NONE
+        }
+      )
+      .addNetworkInterceptor(UserAgentInterceptor(USER_AGENT))
+      .build()
+
+    private const val INTERVAL = 15L
+    private const val INITIAL_DELAY = 0L
     fun startPeriodicWork(appContext: Context?) {
       /*val constraints = Constraints.Builder()
           .setRequiredNetworkType(NetworkType.CONNECTED)
           .build()*/
 
       val workRequest: PeriodicWorkRequest =
-        PeriodicWorkRequestBuilder<UpdateWorkManager>(15, TimeUnit.MINUTES)
-          .setInitialDelay(0, TimeUnit.MINUTES)
-          // .setConstraints(constraints)
+        PeriodicWorkRequestBuilder<UpdateWorkManager>(INTERVAL, TimeUnit.MINUTES)
+          .setInitialDelay(INITIAL_DELAY, TimeUnit.MINUTES)
           .build()
       appContext?.let {
         WorkManager.getInstance(it)
-          .enqueue(workRequest)
-        Log.d("mWorkConfig", "Periodic Worker enqueued...")
+          .enqueueUniquePeriodicWork(
+            "update",
+            ExistingPeriodicWorkPolicy.KEEP,
+            workRequest
+          )
       }
     }
   }
