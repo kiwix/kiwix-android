f8c77fd16f9b58764fa811d12d293edbacb603d1
Jas <jaskq@proton.me>
2025-12-16 01:46:06 +0530

    Added Work manager with the support of parameter dependency injection

--- core/src/main/java/org/kiwix/kiwixmobile/core/CoreApp.kt
+++ core/src/main/java/org/kiwix/kiwixmobile/core/CoreApp.kt
@@ -32,7 +32,7 @@ import org.kiwix.kiwixmobile.core.di.components.CoreComponent
 import org.kiwix.kiwixmobile.core.di.components.DaggerCoreComponent
 import org.kiwix.kiwixmobile.core.main.CoreMainActivity
 import org.kiwix.kiwixmobile.core.utils.files.FileLogger
-import org.kiwix.kiwixmobile.core.utils.workManager.UpdateWorkManager
+import org.kiwix.kiwixmobile.core.utils.workManager.UpdateWorkerFactory
 import javax.inject.Inject
 
 @Suppress("UnnecessaryAbstractClass")
@@ -72,7 +72,6 @@ abstract class CoreApp : Application() {
 
   override fun onCreate() {
     super.onCreate()
-    UpdateWorkManager.startPeriodicWork(this)
     instance = this
     coreComponent = DaggerCoreComponent.builder()
       .context(this)
--- core/src/main/java/org/kiwix/kiwixmobile/core/utils/workManager/UpdateWorkManager.kt
+++ core/src/main/java/org/kiwix/kiwixmobile/core/utils/workManager/UpdateWorkManager.kt
@@ -1,79 +1,38 @@
+/*
+ * Kiwix Android
+ * Copyright (c) 2025 Kiwix <android.kiwix.org>
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
 package org.kiwix.kiwixmobile.core.utils.workManager
 
 import android.content.Context
 import androidx.work.CoroutineWorker
-import androidx.work.ExistingPeriodicWorkPolicy
-import androidx.work.PeriodicWorkRequest
-import androidx.work.PeriodicWorkRequestBuilder
-import androidx.work.WorkManager
 import androidx.work.WorkerParameters
 import dagger.assisted.Assisted
+import dagger.assisted.AssistedFactory
 import dagger.assisted.AssistedInject
-import okhttp3.OkHttpClient
-import okhttp3.logging.HttpLoggingInterceptor
-import okhttp3.logging.HttpLoggingInterceptor.Level.BASIC
-import okhttp3.logging.HttpLoggingInterceptor.Level.NONE
-import org.kiwix.kiwixmobile.core.BuildConfig
-import org.kiwix.kiwixmobile.core.data.remote.KiwixService
-import org.kiwix.kiwixmobile.core.data.remote.UserAgentInterceptor
-import org.kiwix.kiwixmobile.core.di.modules.CALL_TIMEOUT
-import org.kiwix.kiwixmobile.core.di.modules.CONNECTION_TIMEOUT
-import org.kiwix.kiwixmobile.core.di.modules.KIWIX_UPDATE_URL
-import org.kiwix.kiwixmobile.core.di.modules.READ_TIMEOUT
-import org.kiwix.kiwixmobile.core.di.modules.USER_AGENT
-import java.util.concurrent.TimeUnit
-import java.util.concurrent.TimeUnit.SECONDS
 
-@Suppress()
 class UpdateWorkManager @AssistedInject constructor(
-  @Assisted appContext: Context,
-  @Assisted workerParams: WorkerParameters,
-  private var kiwixService: KiwixService
-) : CoroutineWorker(appContext, workerParams) {
-  override suspend fun doWork(): Result {
-    kiwixService =
-      KiwixService.ServiceCreator.newHackListService(
-        okHttpClient = getOkHttpClient(),
-        KIWIX_UPDATE_URL
-      )
-    return Result.success()
-  }
+  @Assisted private val appContext: Context,
+  @Assisted private val params: WorkerParameters,
+) : CoroutineWorker(appContext, params) {
+  override suspend fun doWork(): Result = Result.success()
 
-  @Suppress()
-  companion object {
-    fun getOkHttpClient() = OkHttpClient().newBuilder()
-      .followRedirects(true)
-      .followSslRedirects(true)
-      .connectTimeout(CONNECTION_TIMEOUT, SECONDS)
-      .readTimeout(READ_TIMEOUT, SECONDS)
-      .callTimeout(CALL_TIMEOUT, SECONDS)
-      .addNetworkInterceptor(
-        HttpLoggingInterceptor().apply {
-          level = if (BuildConfig.DEBUG) BASIC else NONE
-        }
-      )
-      .addNetworkInterceptor(UserAgentInterceptor(USER_AGENT))
-      .build()
-
-    private const val INTERVAL = 15L
-    private const val INITIAL_DELAY = 0L
-    fun startPeriodicWork(appContext: Context?) {
-      /*val constraints = Constraints.Builder()
-          .setRequiredNetworkType(NetworkType.CONNECTED)
-          .build()*/
-
-      val workRequest: PeriodicWorkRequest =
-        PeriodicWorkRequestBuilder<UpdateWorkManager>(INTERVAL, TimeUnit.MINUTES)
-          .setInitialDelay(INITIAL_DELAY, TimeUnit.MINUTES)
-          .build()
-      appContext?.let {
-        WorkManager.getInstance(it)
-          .enqueueUniquePeriodicWork(
-            "update",
-            ExistingPeriodicWorkPolicy.KEEP,
-            workRequest
-          )
-      }
-    }
+  @AssistedFactory
+  interface Factory {
+    fun create(appContext: Context, params: WorkerParameters): UpdateWorkManager
   }
 }
