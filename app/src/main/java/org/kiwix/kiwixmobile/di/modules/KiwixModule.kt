fb5e867b3bbd8934d10306e1c2b9e85979af76d8
itsPronay <pronaycoding@gmail.com>
2026-01-24 22:55:00 +0600

    Migrated LocalFileTransferScreen to Compose
    fixed di error in permission helper
    
    fix bugs
    
    fix bugs
    
    Migrated
    
    FIX
    
    FIX
    
    FIX
    
    FIX

--- app/src/main/java/org/kiwix/kiwixmobile/di/modules/KiwixModule.kt
+++ app/src/main/java/org/kiwix/kiwixmobile/di/modules/KiwixModule.kt
@@ -18,6 +18,7 @@
 
 package org.kiwix.kiwixmobile.di.modules
 
+import android.app.Activity
 import android.content.Context
 import android.location.LocationManager
 import android.net.wifi.WifiManager
