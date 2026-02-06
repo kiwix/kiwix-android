554af6895302153fc12749ce74b9588b9cb6d185
atharvyadav22 <dev.atharvyadav@gmail.com>
2026-02-06 19:47:30 +0530

    Fixed Media Saving Issue updated Test Cases
    # Conflicts:
    #	core/src/main/java/org/kiwix/kiwixmobile/core/utils/files/FileUtils.kt

---
+++ core/src/main/java/org/kiwix/kiwixmobile/core/utils/files/SaveResult.kt
@@ -0,0 +1,28 @@
+/*
+ * Kiwix Android
+ * Copyright (c) 2026 Kiwix <android.kiwix.org>
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+package org.kiwix.kiwixmobile.core.utils.files
+
+import android.net.Uri
+import java.io.File
+
+sealed class SaveResult {
+  data class FileSaved(val file: File) : SaveResult()
+  data class MediaSaved(val uri: Uri, val displayName: String) : SaveResult()
+  data class Error(val message: String, val throwable: Throwable? = null) : SaveResult()
+}
