005dff39f72e51297748cc2238d52e08dc18db06
jas <jaskq@proton.me>
2026-02-11 01:17:45 +0530

    ui refinement

--- app/src/main/java/org/kiwix/kiwixmobile/update/UpdateScreen.kt
+++ app/src/main/java/org/kiwix/kiwixmobile/update/UpdateScreen.kt
@@ -19,7 +19,6 @@
 package org.kiwix.kiwixmobile.update
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -30,19 +29,20 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Close
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.minimumInteractiveComponentSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.semantics.semantics
+import androidx.compose.ui.semantics.testTag
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -51,15 +51,18 @@ import com.tonyodev.fetch2.Error
 import com.tonyodev.fetch2.Status
 import org.kiwix.kiwixmobile.R.drawable
 import org.kiwix.kiwixmobile.core.ui.components.ContentLoadingProgressBar
+import org.kiwix.kiwixmobile.core.ui.components.KiwixAppBar
 import org.kiwix.kiwixmobile.core.ui.components.KiwixButton
 import org.kiwix.kiwixmobile.core.ui.components.KiwixSnackbarHost
 import org.kiwix.kiwixmobile.core.ui.components.ProgressBarStyle
 import org.kiwix.kiwixmobile.core.utils.ComposeDimens.FIVE_DP
 import org.kiwix.kiwixmobile.core.utils.ComposeDimens.ONE_DP
-import org.kiwix.kiwixmobile.update.viewmodel.DownloadApkState
+import org.kiwix.kiwixmobile.core.utils.ComposeDimens.TWO_DP
+import org.kiwix.kiwixmobile.nav.destination.library.online.DOWNLOADING_STOP_BUTTON_TESTING_TAG
 import org.kiwix.kiwixmobile.update.viewmodel.UpdateEvents
 import org.kiwix.kiwixmobile.update.viewmodel.UpdateStates
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Suppress("all")
 @Composable
 fun UpdateScreen(
@@ -67,15 +70,22 @@ fun UpdateScreen(
   events: (UpdateEvents) -> Unit = {},
   onUpdateClick: () -> Unit = {},
   onUpdateCancel: () -> Unit = {},
-  onInstallApk: () -> Unit = {}
+  onInstallApk: () -> Unit = {},
+  navigationIcon: @Composable() () -> Unit
 ) {
-  LaunchedEffect(state.downloadApkState.currentDownloadState) {
+  /*LaunchedEffect(state.downloadApkState.currentDownloadState) {
     if (state.downloadApkState.currentDownloadState == Status.COMPLETED) {
       onInstallApk()
     }
-  }
+  }*/
   Scaffold(
     snackbarHost = { KiwixSnackbarHost(snackbarHostState = state.snackbarHostState) },
+    topBar = {
+      KiwixAppBar(
+        title = "Update",
+        navigationIcon = navigationIcon,
+      )
+    }
   ) {
     UpdateInfoCard(
       state = state,
@@ -102,35 +112,9 @@ fun UpdateInfoCard(
       .fillMaxSize(),
     verticalArrangement = Arrangement.Center
   ) {
-    Row(
-      verticalAlignment = Alignment.Top
-    ) {
-      Image(
-        modifier = Modifier.size(50.dp),
-        painter = painterResource(drawable.kiwix_icon),
-        contentDescription = null
-      )
-      Spacer(modifier = Modifier.width(16.dp))
-      Column {
-        LabelText(
-          label = "Update available for Kiwix",
-          style = MaterialTheme.typography.titleMedium,
-          color = Color.Black
-        )
-        Spacer(modifier = Modifier.height(4.dp))
-        LabelText(
-          label = "Download size: 1.4 MB",
-          style = MaterialTheme.typography.bodySmall,
-          color = Color.Gray
-        )
-      }
-    }
+    AppInfoRow()
+
     Spacer(modifier = Modifier.height(16.dp))
-    LabelText(
-      label = "Download the latest version",
-      style = MaterialTheme.typography.bodyMedium,
-      color = Color.DarkGray
-    )
 
     val downloadApkState = state.downloadApkState
     when (downloadApkState.currentDownloadState) {
@@ -170,12 +154,34 @@ fun UpdateInfoCard(
 }
 
 @Composable
+fun AppInfoRow() {
+  Row(
+    verticalAlignment = Alignment.Top
+  ) {
+    Image(
+      modifier = Modifier.size(50.dp),
+      painter = painterResource(drawable.kiwix_icon),
+      contentDescription = null
+    )
+    Spacer(modifier = Modifier.width(16.dp))
+    Column {
+      LabelText(
+        label = "Update available for Kiwix"
+      )
+      Spacer(modifier = Modifier.height(4.dp))
+      DownloadText(
+        text = "Download size: 1.4 MB"
+      )
+    }
+  }
+}
+
+@Composable
 fun DownloadInfoRow(
   state: UpdateStates,
   onCancel: () -> Unit,
   modifier: Modifier = Modifier,
 ) {
-  val contentColor = Color.Gray
   // Automatically invoke onStopClick if the download failed
   LaunchedEffect(state.downloadApkState.currentDownloadState) {
     if (state.downloadApkState.currentDownloadState == Status.FAILED) {
@@ -210,12 +216,10 @@ fun DownloadInfoRow(
         horizontalArrangement = Arrangement.SpaceBetween
       ) {
         DownloadText(
-          text = " ",
-          contentColor = contentColor
+          text = "12/12"
         )
         DownloadText(
-          text = " ",
-          contentColor = contentColor
+          text = "12 min"
         )
       }
 
@@ -231,39 +235,40 @@ fun DownloadInfoRow(
 
     Spacer(modifier = Modifier.width(16.dp))
 
-    Icon(
-      imageVector = Icons.Default.Close,
-      contentDescription = "Cancel upload",
-      tint = contentColor,
+    IconButton(
+      onClick = onCancel,
       modifier = Modifier
-        .size(20.dp)
-        .clickable(onClick = onCancel)
-    )
+        .minimumInteractiveComponentSize()
+        .padding(horizontal = TWO_DP)
+        .semantics { testTag = DOWNLOADING_STOP_BUTTON_TESTING_TAG }
+    ) {
+      Icon(
+        painter = painterResource(id = org.kiwix.kiwixmobile.R.drawable.ic_stop_24dp),
+        contentDescription = null
+      )
+    }
   }
 }
 
 @Composable
 fun LabelText(
-  label: String,
-  style: TextStyle = MaterialTheme.typography.bodyMedium,
-  color: Color = Color.DarkGray
+  label: String
 ) {
   Text(
     text = label,
-    style = style,
-    color = color
+    style = MaterialTheme.typography.titleMedium,
+    color = MaterialTheme.colorScheme.onSurfaceVariant
   )
 }
 
 @Composable
 fun DownloadText(
   text: String,
-  contentColor: Color
 ) {
   Text(
     text = text,
     fontSize = 12.sp,
-    color = contentColor,
+    color = MaterialTheme.colorScheme.onSurfaceVariant,
     fontWeight = FontWeight.Medium
   )
 }
@@ -271,5 +276,12 @@ fun DownloadText(
 @Preview
 @Composable
 fun UpdateScreenPreview() {
-  UpdateScreen(state = UpdateStates(downloadApkState = DownloadApkState()))
+  UpdateScreen(
+    state = UpdateStates(),
+    events = {},
+    onUpdateClick = {},
+    onUpdateCancel = {},
+    onInstallApk = {},
+    {}
+  )
 }
