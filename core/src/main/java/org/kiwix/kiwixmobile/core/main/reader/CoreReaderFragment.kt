0e0fe689b4bac0b613c7cc88b711753929fb2085
Aryan Bhati <codiearyan07@gmail.com>
2026-02-28 03:54:27 +0530

    fix: added error log on pdf print fail

--- core/src/main/java/org/kiwix/kiwixmobile/core/main/reader/CoreReaderFragment.kt
+++ core/src/main/java/org/kiwix/kiwixmobile/core/main/reader/CoreReaderFragment.kt
@@ -1473,7 +1473,10 @@ abstract class CoreReaderFragment :
         adapter = printAdapter,
         outputFile = pdfFile,
         onComplete = { if (isAdded) sharePdfFile(it) },
-        onError = { if (isAdded) context?.toast(string.unable_to_share_article, Toast.LENGTH_SHORT) },
+        onError = {
+          Log.e(TAG_KIWIX, "Failed to generate PDF for sharing: $it")
+          if (isAdded) context?.toast(string.unable_to_share_article, Toast.LENGTH_SHORT)
+        },
       )
     }
   }
