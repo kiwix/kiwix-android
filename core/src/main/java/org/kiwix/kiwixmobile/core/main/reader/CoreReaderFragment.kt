9e0d0d5f29c8dae129c19e194c42f9d3e114a91a
Aryan Bhati <codiearyan07@gmail.com>
2026-02-28 03:48:34 +0530

    feat: added a toast indicator if page not fully loaded

--- core/src/main/java/org/kiwix/kiwixmobile/core/main/reader/CoreReaderFragment.kt
+++ core/src/main/java/org/kiwix/kiwixmobile/core/main/reader/CoreReaderFragment.kt
@@ -1459,6 +1459,10 @@ abstract class CoreReaderFragment :
   override fun onShareMenuClicked() {
     runSafelyInCoreReaderLifecycleScope {
       val webView = getCurrentWebView() ?: return@runSafelyInCoreReaderLifecycleScope
+      if (webView.progress < HUNDERED) {
+        context?.toast(string.please_wait_for_page_to_load, Toast.LENGTH_SHORT)
+        return@runSafelyInCoreReaderLifecycleScope
+      }
       val title = webView.title ?: "Article"
       val slugifiedTitle = title.toSlug().ifEmpty { "article" }
       val pdfFile = java.io.File(requireContext().cacheDir, "$slugifiedTitle.pdf")
