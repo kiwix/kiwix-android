fdbd453b02e3bfe21114b57bc0947a42ceb1ec5d
MohitMaliFtechiz <futuretechnoworld.mohit@gmail.com>
2026-03-07 18:10:38 +0530

    Fixed: `CopyMoveFileHandlerTest` failures.
    * Fixed failing tests `DetectingFileSystem with file larger than 4GB observes filesystem` and `CannotWrite4GbFile shows filesystem limitation error`.
    * Fixed failing tests DetectingFileSystem with file larger than 4GB observes filesystem and CannotWrite4GbFile shows filesystem limitation error.
    * Updated the logic to observe the filesystem state only once, since continuous observation is unnecessary for this use case.

--- app/src/main/java/org/kiwix/kiwixmobile/nav/destination/library/CopyMoveFileHandler.kt
+++ app/src/main/java/org/kiwix/kiwixmobile/nav/destination/library/CopyMoveFileHandler.kt
@@ -233,12 +233,13 @@ class CopyMoveFileHandler @Inject constructor(
   fun observeFileSystemState() {
     if (storageObservingJob?.isActive == true) return
     storageObservingJob = lifecycleScope?.launch {
-      fat32Checker.fileSystemStates.collect {
-        copyMoveProgressBarController.hidePreparingCopyMoveDialog()
-        if (validateZimFileCanCopyOrMove()) {
-          performCopyMoveOperation()
-        }
+      // Wait until filesystem detection completes
+      fat32Checker.fileSystemStates.first { it != DetectingFileSystem }
+      copyMoveProgressBarController.hidePreparingCopyMoveDialog()
+      if (validateZimFileCanCopyOrMove()) {
+        performCopyMoveOperation()
       }
+      storageObservingJob?.cancel()
     }
   }
 
