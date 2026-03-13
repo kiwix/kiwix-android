ffd2027c67baac50e29357b19e6409ee12fc51c8
atharvyadav22 <dev.atharvyadav@gmail.com>
2026-03-13 18:16:15 +0530

    Introduced Macrobenchmarking with Startup Test

---
+++ benchmark/src/main/java/org/kiwix/kiwixmobile/benchmark/KiwixAppStartupBenchmark.kt
@@ -0,0 +1,54 @@
+package org.kiwix.kiwixmobile.benchmark
+
+import androidx.benchmark.macro.CompilationMode
+import androidx.benchmark.macro.StartupMode
+import androidx.benchmark.macro.StartupTimingMetric
+import androidx.benchmark.macro.junit4.MacrobenchmarkRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class KiwixAppStartupBenchmark {
+  @get:Rule
+  val benchmarkRule = MacrobenchmarkRule()
+
+  // Cold start: App process is not in memory.
+  @Test
+  fun startupCold() = benchmarkRule.measureRepeated(
+    packageName = "org.kiwix.kiwixmobile",
+    metrics = listOf(StartupTimingMetric()),
+    iterations = 5,
+    startupMode = StartupMode.COLD,
+    compilationMode = CompilationMode.Partial()
+  ) {
+    pressHome()
+    startActivityAndWait()
+  }
+
+  // Warm start: App process exists but activity is recreated.
+  @Test
+  fun startupWarm() = benchmarkRule.measureRepeated(
+    packageName = "org.kiwix.kiwixmobile",
+    metrics = listOf(StartupTimingMetric()),
+    iterations = 5,
+    startupMode = StartupMode.WARM,
+    compilationMode = CompilationMode.Partial()
+  ) {
+    pressHome()
+    startActivityAndWait()
+  }
+
+  // Hot start: App process exists just activity needs to be brought from background to foreground.
+  @Test
+  fun startupHot() = benchmarkRule.measureRepeated(
+    packageName = "org.kiwix.kiwixmobile",
+    metrics = listOf(StartupTimingMetric()),
+    iterations = 5,
+    startupMode = StartupMode.HOT,
+    compilationMode = CompilationMode.Partial()
+  ) {
+    startActivityAndWait()
+  }
+}
