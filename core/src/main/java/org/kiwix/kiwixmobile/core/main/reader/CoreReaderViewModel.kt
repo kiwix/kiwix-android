cefdc72df095d90169286aaddce1e72da3ca9343
Atharv Yadav <dev.atharvyadav@gmail.com>
2026-08-05 14:13:07 +0530

    omplete Reader Action Branch Coverage NavigationHistoryItemClick  * SelectTab  * OpenSearch  * CloseTab  * FindInPageQueryChanged  * FindInPageNextClicked  * FindInPagePreviousClicked  * FindInPageCloseClicked

--- core/src/main/java/org/kiwix/kiwixmobile/core/main/reader/CoreReaderViewModel.kt
+++ core/src/main/java/org/kiwix/kiwixmobile/core/main/reader/CoreReaderViewModel.kt
@@ -972,7 +972,7 @@ abstract class CoreReaderViewModel(
       )
     }
 
-  protected suspend fun selectTab(position: Int) {
+  protected open suspend fun selectTab(position: Int) {
     readerWebViewManager.setCurrentWebViewIndex(position)
     updateBottomToolbarVisibility()
     updateUrlFlow()
