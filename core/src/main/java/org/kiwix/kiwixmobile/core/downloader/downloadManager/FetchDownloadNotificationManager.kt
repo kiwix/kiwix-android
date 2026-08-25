e160473bde8ec79b355943a5e883afc51abf122e
Atharv Yadav <dev.atharvyadav@gmail.com>
2026-08-25 16:58:24 +0530

    Fixed Queued state showing Pause(Device is offline) instead of Pause as Queue is for downlaod limit notification

--- core/src/main/java/org/kiwix/kiwixmobile/core/downloader/downloadManager/FetchDownloadNotificationManager.kt
+++ core/src/main/java/org/kiwix/kiwixmobile/core/downloader/downloadManager/FetchDownloadNotificationManager.kt
@@ -148,7 +148,7 @@ class FetchDownloadNotificationManager @Inject constructor(
       downloadNotification.isCompleted -> context.getString(R.string.complete)
       downloadNotification.isFailed -> context.getString(R.string.download_failed_state)
       downloadNotification.isQueued -> buildSubtitle(
-        context.getString(R.string.paused_offline_state),
+        context.getString(R.string.resuming_state),
         downloadNotification.downloaded,
         downloadNotification.total
       )
@@ -255,11 +255,6 @@ class FetchDownloadNotificationManager @Inject constructor(
 
       downloadNotification.isQueued ->
         notificationBuilder.setTimeoutAfter(DEFAULT_NOTIFICATION_TIMEOUT_AFTER_RESET)
-          .addAction(
-            drawable.fetch_notification_pause,
-            context.getString(R.string.notification_pause_button_text),
-            null
-          )
           .addAction(
             drawable.fetch_notification_cancel,
             context.getString(R.string.cancel),
